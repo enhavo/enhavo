ff52e3448836b1d1f8e50e2f4270a8d608dec689
Gerhard Seidel <gseidel.message@googlemail.com>
2015-01-15 01:19:44 +0100

    Squashed 'src/esperanto/NewsBundle/' content from commit 0da7907
    git-subtree-dir: src/esperanto/NewsBundle
    git-subtree-split: 0da79070bbb56f1d81b1ffa14805a69e7c6f81b4

---
+++ Service/NewsResolver.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Michael
+ * Date: 02.09.14
+ * Time: 18:26
+ */
+
+namespace esperanto\NewsBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormFactory;
+use esperanto\NewsBundle\Form\Type\NewsType;
+
+class NewsResolver
+{
+    protected $container;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
+
+    public function getNews(Request $request)
+    {
+        if($request->get('id') == 'preview' && $request->getMethod() === 'POST')
+        {
+            $news = $this->getPreviewNews($request);
+        } else {
+            $news = $this->getLiveNews($request);
+        }
+
+        return $news;
+    }
+
+    public function getPreviewNews(Request $request)
+    {
+        /** @var $formFactory FormFactory */
+        $formFactory = $this->container->get('form.factory');
+        $form = $formFactory->create('esperanto_news_news');
+        $form->submit($request);
+        return $form->getData();
+    }
+
+    public function getLiveNews(Request $request)
+    {
+        /** @var $doctrine EntityManager */
+        $doctrine = $this->container->get('doctrine');
+        $id = $request->get('id');
+
+        $repository = $this->container->get('esperanto_news.repository.news');
+        $news = $repository->find($id);
+        return $news;
+    }
+} 
