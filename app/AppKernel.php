a8bb93a5da7d07efea633fb89dbb4ae4a749efeb
Gerhard Seidel <gseidel.message@googlemail.com>
2015-05-20 12:00:29 +0200

    adding newsletter bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -46,6 +46,7 @@ class AppKernel extends Kernel
             new esperanto\DownloadBundle\esperantoDownloadBundle(),
 
             new esperanto\ProjectBundle\esperantoProjectBundle(),
+            new esperanto\NewsletterBundle\esperantoNewsletterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
