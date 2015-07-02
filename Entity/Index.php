308de67751b2648e8de371f36f531f7d9d3d1b67
Gerhard Seidel <gseidel.message@googlemail.com>
2015-07-02 23:22:16 +0200

    Squashed 'src/Enhavo/Bundle/SearchBundle/' content from commit c444e0f
    git-subtree-dir: src/Enhavo/Bundle/SearchBundle
    git-subtree-split: c444e0f85ea7579401288eb29e24a4e0f9d9240e

---
+++ Entity/Index.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace esperanto\SearchBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Index
+ */
+class Index
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $teaser;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var string
+     */
+    private $route;
+
+    /**
+     * @var string
+     */
+    private $routeParameter;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Index
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set teaser
+     *
+     * @param string $teaser
+     * @return Index
+     */
+    public function setTeaser($teaser)
+    {
+        $this->teaser = $teaser;
+
+        return $this;
+    }
+
+    /**
+     * Get teaser
+     *
+     * @return string 
+     */
+    public function getTeaser()
+    {
+        return $this->teaser;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return Index
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set route
+     *
+     * @param string $route
+     * @return Index
+     */
+    public function setRoute($route)
+    {
+        $this->route = $route;
+
+        return $this;
+    }
+
+    /**
+     * Get route
+     *
+     * @return string 
+     */
+    public function getRoute()
+    {
+        return $this->route;
+    }
+
+    /**
+     * Set routeParameter
+     *
+     * @param string $routeParameter
+     * @return Index
+     */
+    public function setRouteParameter($routeParameter)
+    {
+        $this->routeParameter = serialize($routeParameter);
+
+        return $this;
+    }
+
+    /**
+     * Get routeParameter
+     *
+     * @return string 
+     */
+    public function getRouteParameter()
+    {
+        return unserialize($this->routeParameter);
+    }
+}
---
+++ Search/SearchManager.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * SearchManager.php
+ *
+ * @since 04/10/14
+ * @author Gerhard Seidel <gseidel.message@googlemail.com>
+ */
+
+namespace esperanto\SearchBundle\Search;
+
+
+use Doctrine\Common\Persistence\ObjectManager;
+use esperanto\SearchBundle\Entity\Index;
+
+class SearchManager
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $manager;
+
+    public function __construct(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    public function index(SearchIndexInterface $index)
+    {
+        $searchIndex = $this->getIndex($index);
+
+        if(empty($searchIndex)) {
+            $searchIndex = $this->createIndex();
+            $this->manager->persist($searchIndex);
+            $searchIndex->setRoute($index->getIndexRoute());
+            $searchIndex->setRouteParameter($index->getIndexRouteParameter());
+        }
+
+        $searchIndex->setContent($index->getIndexContent());
+        $searchIndex->setTeaser($index->getIndexTeaser());
+        $searchIndex->setTitle($index->getIndexTitle());
+
+        $this->manager->flush();
+    }
+
+    public function search($query)
+    {
+        return $this->manager->getRepository('esperantoSearchBundle:Index')->search($query, 20);
+    }
+
+    protected function getIndex(SearchIndexInterface $index)
+    {
+        $repository = $this->manager->getRepository('esperantoSearchBundle:Index');
+        $indexList = $repository->findBy(array('route' => $index->getIndexRoute()));
+        if(count($indexList)) {
+            foreach($indexList as $item) {
+                $routeParameter = $item->getRouteParameter();
+                if($this->equalsRouteParameter($routeParameter, $index->getIndexRouteParameter())) {
+                    return $item;
+                }
+            }
+        }
+        return null;
+    }
+
+    protected function equalsRouteParameter($one, $two)
+    {
+        return !count(array_diff($one, $two));
+    }
+
+    /**
+     * @return Index
+     */
+    protected function createIndex()
+    {
+        return new Index();
+    }
+} 
