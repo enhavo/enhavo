4b1bc1536339ae5ea0b2489e61943c29a5d03d67
Gerhard Seidel <gseidel.message@googlemail.com>
2015-07-02 23:22:19 +0200

    Squashed 'src/Enhavo/Bundle/SliderBundle/' content from commit ac32659
    git-subtree-dir: src/Enhavo/Bundle/SliderBundle
    git-subtree-split: ac3265967da992193a006e81288f5e996b9f4e2c

---
+++ Entity/Slider.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace esperanto\SliderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Slider
+ */
+class Slider
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     */
+    private $slides;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->slides = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Slider
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Add slides
+     *
+     * @param \esperanto\ProjectBundle\Entity\Slide $slides
+     * @return Slider
+     */
+    public function addSlide(\esperanto\SliderBundle\Entity\Slide $slides)
+    {
+        $this->slides[] = $slides;
+        $slides->setSlider($this);
+        return $this;
+    }
+
+    /**
+     * Remove slides
+     *
+     * @param \esperanto\ProjectBundle\Entity\Slide $slides
+     */
+    public function removeSlide(\esperanto\SliderBundle\Entity\Slide $slides)
+    {
+        $this->slides->removeElement($slides);
+    }
+
+    /**
+     * Get slides
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getSlides()
+    {
+        return $this->slides;
+    }
+}
