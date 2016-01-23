9f68063a22a36e1ef07f9b9d6267644a82db07ad
Jennifer Helbing <jennifer.helbing@xq-web.de>
2016-01-23 16:46:33 +0100

    SearchBundle add search files

---
+++ src/Enhavo/Bundle/SearchBundle/EventListener/SaveSubscriber.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jhelbing
+ * Date: 23.01.16
+ * Time: 16:03
+ */
+
+class SaveSubscriber {
+
+}
---
+++ src/Enhavo/Bundle/SearchBundle/Index/IndexEngine.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jhelbing
+ * Date: 23.01.16
+ * Time: 16:00
+ */
+
+class IndexEngine {
+
+    public function index($model)
+    {
+
+    }
+}
