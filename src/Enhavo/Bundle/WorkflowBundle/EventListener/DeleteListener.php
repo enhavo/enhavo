3145966bc8dccae0dfaee9fa8c422fe4a987154c
Jennifer Helbing <jennifer.helbing@xq-web.de>
2016-04-13 10:18:33 +0200

    workflow deleteListener changed

--- src/Enhavo/Bundle/WorkflowBundle/EventListener/DeleteListener.php
+++ src/Enhavo/Bundle/WorkflowBundle/EventListener/DeleteListener.php
@@ -3,11 +3,11 @@
 namespace Enhavo\Bundle\WorkflowBundle\EventListener;
 
 use Symfony\Component\Yaml\Parser;
-
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 class DeleteListener
 {
-    public function onDelete($event)
+    public function onDelete(GenericEvent $event)
     {
         $test = $event;
     }
