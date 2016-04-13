bfc3fce255bd7c39e28b94e321906824986b7ad3
Jennifer Helbing <jennifer.helbing@xq-web.de>
2016-04-13 09:48:14 +0200

    deleteListener for workflow added

---
+++ src/Enhavo/Bundle/WorkflowBundle/EventListener/DeleteListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Enhavo\Bundle\WorkflowBundle\EventListener;
+
+use Symfony\Component\Yaml\Parser;
+
+
+class DeleteListener
+{
+    public function onDelete($event)
+    {
+
+    }
+}
