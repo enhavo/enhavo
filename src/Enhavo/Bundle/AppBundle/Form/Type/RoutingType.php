a9d8008d315c6a5cc307691187edfbe242f967d4
Gerhard Seidel <gseidel.message@googlemail.com>
2016-05-16 18:06:33 +0200

    add url guesser

--- src/Enhavo/Bundle/AppBundle/Form/Type/RoutingType.php
+++ src/Enhavo/Bundle/AppBundle/Form/Type/RoutingType.php
@@ -8,6 +8,7 @@
 
 namespace Enhavo\Bundle\AppBundle\Form\Type;
 
+use Enhavo\Bundle\AppBundle\Entity\Route;
 use Enhavo\Bundle\AppBundle\Route\Routeable;
 use Enhavo\Bundle\AppBundle\Route\Routing;
 use Enhavo\Bundle\AppBundle\Route\Slugable;
@@ -17,6 +18,7 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Routing\RouterInterface;
+use Enhavo\Bundle\AppBundle\Route\RouteGuesser;
 
 class RoutingType extends AbstractType
 {
@@ -25,16 +27,20 @@ class RoutingType extends AbstractType
      */
     protected $router;
 
-    public function __construct(RouterInterface $router)
+    /**
+     * @var RouteGuesser
+     */
+    protected $guesser;
+
+    public function __construct(RouterInterface $router, RouteGuesser $guesser)
     {
         $this->router = $router;
+        $this->guesser = $guesser;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $router = $this->router;
-
-        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) use ($router, $options) {
+        $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event) use ($options) {
             $data = $event->getData();
             $form = $event->getForm();
 
@@ -46,7 +52,7 @@ class RoutingType extends AbstractType
                 if($data->getId() && !empty($options['routing_route'])) {
                     $form->add('link', 'text', array(
                         'mapped' => false,
-                        'data' => $router->generate($options['routing_route'], array(
+                        'data' => $this->router->generate($options['routing_route'], array(
                             'id' => $data->getId()
                         ), true),
                         'disabled' => true
@@ -62,7 +68,7 @@ class RoutingType extends AbstractType
                 if($data->getSlug() && !empty($options['routing_route'])) {
                     $form->add('link', 'text', array(
                         'mapped' => false,
-                        'data' => $router->generate($options['routing_route'], array(
+                        'data' => $this->router->generate($options['routing_route'], array(
                             'slug' => $data->getSlug()
                         ), true),
                         'disabled' => true
@@ -87,7 +93,7 @@ class RoutingType extends AbstractType
                 if($data->getId() && $data->getSlug() && !empty($options['routing_route'])) {
                     $form->add('link', 'text', array(
                         'mapped' => false,
-                        'data' => $router->generate($options['routing_route'], array(
+                        'data' => $this->router->generate($options['routing_route'], array(
                             'id' => $data->getId(),
                             'slug' => $data->getSlug()
                         ), true),
@@ -107,8 +113,24 @@ class RoutingType extends AbstractType
                 }
 
                 $form->add('route', 'enhavo_route');
+
             }
 
+        });
+
+        $builder->addEventListener(FormEvents::SUBMIT, function (FormEvent $event) use ($options) {
+            if ($options['routing_strategy'] === Routing::STRATEGY_ROUTE) {
+                $data = $event->getData();
+                if($data instanceof Routeable) {
+                    $route = $data->getRoute();
+                    if($route instanceof Route && empty($route->getStaticPrefix())) {
+                        $url = $this->guesser->guessUrl($data);
+                        if($url !== null) {
+                            $route->setStaticPrefix($url);
+                        }
+                    }
+                }
+            }
         });
     }
 
