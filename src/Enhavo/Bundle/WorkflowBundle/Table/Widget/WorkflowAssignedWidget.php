4026956046ff25cc6791fcec7565fd2e290664f5
Jennifer Helbing <jennifer.helbing@xq-web.de>
2016-05-17 17:18:38 +0200

    workflow assigned added

---
+++ src/Enhavo/Bundle/WorkflowBundle/Table/Widget/WorkflowAssignedWidget.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Enhavo\Bundle\WorkflowBundle\Table\Widget;
+
+use Enhavo\Bundle\AppBundle\Table\AbstractTableWidget;
+use Enhavo\Bundle\AppBundle\Security\Roles\RoleUtil;
+
+class WorkflowAssignedWidget extends AbstractTableWidget {
+
+    public function render($options, $property, $item)
+    {
+        $templateEngine = $this->container->get('templating');
+        $securityContext = $this->container->get('security.context');
+        $roleUtil = new RoleUtil();
+        $roleName = $roleUtil->getRoleName($item, RoleUtil::ACTION_UPDATE);
+        $isGranted = false;
+        if($securityContext->isGranted($roleName, $item)){
+            $isGranted = true;
+        }
+        return $templateEngine->render('EnhavoWorkflowBundle:Widget:workflowAssigned.html.twig', array(
+            'userIsGranted' => $isGranted
+        ));
+    }
+
+    public function getType()
+    {
+        return 'workflow_assigned';
+    }
+}
