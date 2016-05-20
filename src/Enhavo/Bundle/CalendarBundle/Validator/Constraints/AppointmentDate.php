6bcdd753acff689c8dcf589d83d11be246baa449
Jennifer Helbing <jennifer.helbing@xq-web.de>
2016-05-20 13:05:07 +0200

    validator for appointment dates added

--- src/Enhavo/Bundle/CalendarBundle/Validator/Constraints/AppointmentDate.php
+++ src/Enhavo/Bundle/CalendarBundle/Validator/Constraints/AppointmentDate.php
@@ -1,11 +1,24 @@
 <?php
+namespace Enhavo\Bundle\CalendarBundle\Validator\Constraints;
 /**
  * Created by PhpStorm.
  * User: jhelbing
  * Date: 20.05.16
  * Time: 12:39
  */
+use Symfony\Component\Validator\Constraint;
 
-class AppointmentDate {
+class AppointmentDate extends Constraint{
 
+    public $datesDoNotMatch = 'appointment.validator.error.dates';
+
+    public function validatedBy()
+    {
+        return 'valid_appointment_date';
+    }
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
 }
--- src/Enhavo/Bundle/CalendarBundle/Validator/Constraints/AppointmentDateValidator.php
+++ src/Enhavo/Bundle/CalendarBundle/Validator/Constraints/AppointmentDateValidator.php
@@ -1,4 +1,5 @@
 <?php
+namespace Enhavo\Bundle\CalendarBundle\Validator\Constraints;
 /**
  * Created by PhpStorm.
  * User: jhelbing
@@ -6,6 +7,29 @@
  * Time: 12:39
  */
 
-class AppointmentDateValidator {
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\DependencyInjection\Container;
 
+class AppointmentDateValidator extends ConstraintValidator
+{
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function validate($appointment, Constraint $constraint)
+    {
+        $translator = $this->container->get('translator');
+        $this->validateDate($appointment->getDateFrom(),$appointment->getDateTo(), $translator, $constraint);
+    }
+
+    protected function validateDate($dateFrom, $dateTo, $translator, $constraint){
+        if($dateTo < $dateFrom){
+            $this->context->buildViolation($translator->trans($constraint->datesDoNotMatch, array(), 'EnhavoCalendarBundle'))
+                ->addViolation();
+        }
+    }
 }
