58bdbad153bdaabcce39113fe97183a47fd01d47
Fabian Liebl <fabian.liebl@xq-web.de>
2016-05-24 13:13:25 +0200

    Fixed style error in batch checkboxes (#114); reactivated batch actions; added batch publish to articles

--- src/Enhavo/Bundle/ArticleBundle/Controller/ArticleController.php
+++ src/Enhavo/Bundle/ArticleBundle/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace Enhavo\Bundle\ArticleBundle\Controller;
 
 use Enhavo\Bundle\AppBundle\Controller\ResourceController;
+use Enhavo\Bundle\ArticleBundle\Entity\Article;
 
 class ArticleController extends ResourceController
 {
@@ -11,5 +12,21 @@ class ArticleController extends ResourceController
         return $this->render('EnhavoArticleBundle:Article:show.html.twig', array(
             'data' => $article
         ));
+    }
+
+    public function batchActionPublish($resources)
+    {
+        $this->isGrantedOr403('edit');
+        $em = $this->get('doctrine.orm.entity_manager');
+        /** @var Article $article */
+        foreach ($resources as $article) {
+            if (!$article->getPublic()) {
+                $article->setPublic(true);
+                $em->persist($article);
+            }
+        }
+        $em->flush();
+
+        return true;
     }
 }
