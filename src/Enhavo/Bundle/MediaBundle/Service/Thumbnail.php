65c0027e13b481a6a63330ebb1587209291b227e
Michael Kellermann <michael.kellermann@xq-web.de>
2016-09-05 18:27:43 +0200

    added support for animated gifs; bugfix png files with transparency

--- src/Enhavo/Bundle/MediaBundle/Service/Thumbnail.php
+++ src/Enhavo/Bundle/MediaBundle/Service/Thumbnail.php
@@ -65,14 +65,36 @@ class Thumbnail
         {
             case( 1 ):
                 // GIF
-                $src_image = imageCreateFromGIF( $input );
-                $dst_image = imageCreateTrueColor( $width, $height );
-                imagecopyresampled ($dst_image, $src_image , $dst_x , $dst_y , $src_x , $src_y , $dst_w , $dst_h , $src_w , $src_h);
-                imageGIF( $dst_image, $output );
-                unset($dst_image);
-                unset($src_image);
-                return true;
-                break;
+                exec("convert -version", $out, $rcode);
+                if($rcode === 0 && self::isGifAnimated($input)) {
+                    $resize_dst_w = max($dst_w,$dst_h);
+                    $resize_dst_h = max($dst_w,$dst_h);
+                    exec("convert ".$input." -size ".$src_w."x".$src_h." -coalesce -resize ".$resize_dst_w."x".$resize_dst_h." ".$output);
+                    $oldRatio = $oldWidth/$oldHeight;
+                    $newRatio = $dst_w/$dst_h;
+                    if($oldRatio > $newRatio) {
+                        $dst_x = abs($dst_w-$resize_dst_w)/2;
+                    } else {
+                        $dst_y = abs($dst_h-$resize_dst_h)/2;
+                    }
+                    exec("convert ".$output." -repage 0x0 -crop ".$dst_w."x".$dst_h."+".$dst_x."+".$dst_y." +repage ".$output);
+                    return true;
+                    break;
+                } else {
+                    $src_image = imageCreateFromGIF( $input );
+                    $dst_image = imageCreateTrueColor( $dst_w, $dst_h );
+
+                    imagecolortransparent($dst_image, imagecolorallocatealpha($dst_image, 0, 0, 0, 127));
+                    imagealphablending($dst_image, false);
+                    imagesavealpha($dst_image, true);
+
+                    imagecopyresampled ($dst_image, $src_image , $dst_x , $dst_y , $src_x , $src_y , $dst_w , $dst_h , $src_w , $src_h);
+                    imageGIF( $dst_image, $output );
+                    unset($dst_image);
+                    unset($src_image);
+                    return true;
+                    break;
+                }
             case( 2 ):
                 // JPEG
                 $src_image = imageCreateFromJPEG( $input );
@@ -87,6 +109,11 @@ class Thumbnail
                 // PNG
                 $src_image = imageCreateFromPNG( $input );
                 $dst_image = imageCreateTrueColor( $width, $height );
+
+                imagecolortransparent($dst_image, imagecolorallocatealpha($dst_image, 0, 0, 0, 127));
+                imagealphablending($dst_image, false);
+                imagesavealpha($dst_image, true);
+
                 imagecopyresampled ($dst_image, $src_image , $dst_x , $dst_y , $src_x , $src_y , $dst_w , $dst_h , $src_w , $src_h);
                 imagePNG( $dst_image, $output, 0);
                 unset($dst_image);
@@ -96,5 +123,34 @@ class Thumbnail
             default:
                 return false;
         }
+    }
+
+    static public function isGifAnimated($filepath) {
+        $filecontents = file_get_contents($filepath);
+
+        $str_loc = 0;
+        $count = 0;
+
+        while ($count < 2) {
+
+            $where1 = strpos($filecontents,"\x00\x21\xF9\x04",$str_loc);
+
+            if ($where1 === FALSE) {
+                break;
+            } else {
+                $str_loc = $where1+1;
+                $where2 = strpos($filecontents,"\x00\x2C",$str_loc);
+                if ($where2 === FALSE) {
+                    break;
+                } else {
+                    if ($where1+8 == $where2) {
+                        $count++;
+                    }
+                    $str_loc = $where2+1;
+                }
+            }
+        }
+
+        return $count > 1;
     }
 }
