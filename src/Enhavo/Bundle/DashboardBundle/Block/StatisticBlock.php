b983886012dd5c1cf433b080b03de6da5aa033f4
Gerhard Seidel <gseidel.message@googlemail.com>
2017-05-17 12:27:43 +0200

    add statitstic block with article provider

---
+++ src/Enhavo/Bundle/DashboardBundle/Block/StatisticBlock.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * StatisticBlock.php
+ *
+ * @since 17/05/17
+ * @author gseidel
+ */
+
+namespace Enhavo\Bundle\DashboardBundle\Block;
+
+
+use Enhavo\Bundle\AppBundle\Block\BlockInterface;
+use Enhavo\Bundle\AppBundle\Type\AbstractType;
+
+class StatisticBlock extends AbstractType implements BlockInterface
+{
+    public function render($parameters)
+    {
+        $providers = $this->getRequiredOption('providers', $parameters);
+        $collector = $this->container->get('enhavo_dashboard.statistic_collector');
+
+        $providerServices = [];
+        foreach($providers as $provider) {
+            $providerServices[] = $collector->getType($provider);
+        }
+
+        return $this->renderTemplate('EnhavoDashboardBundle:Block:statistic.html.twig', [
+            'providers' => $providerServices
+        ]);
+    }
+
+    public function getType()
+    {
+        return 'statistic';
+    }
+}
--- src/Enhavo/Bundle/DashboardBundle/EnhavoDashboardBundle.php
+++ src/Enhavo/Bundle/DashboardBundle/EnhavoDashboardBundle.php
@@ -2,8 +2,18 @@
 
 namespace Enhavo\Bundle\DashboardBundle;
 
+use Enhavo\Bundle\AppBundle\Type\TypeCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EnhavoDashboardBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(
+            new TypeCompilerPass('enhavo_dashboard.statistic_collector', 'enhavo.statistic')
+        );
+    }
 }
