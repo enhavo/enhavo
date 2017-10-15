5d7c03218b72346fab6b6ef7e978d64261b099ca
Gerhard Seidel <gseidel.message@googlemail.com>
2017-10-15 22:55:50 +0200

    fix missing translation for open graph property

---
+++ src/Enhavo/Bundle/TranslationBundle/Exception/TranslationException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gseidel
+ * Date: 15.10.17
+ * Time: 22:35
+ */
+
+namespace Enhavo\Bundle\TranslationBundle\Exception;
+
+
+class TranslationException extends \Exception
+{
+
+}
