bd227ef61cab3e77e7f8df67586e6930e6f64d92
Fabian Liebl <fabian.liebl@xq-web.de>
2018-09-18 11:19:50 +0200

    [MediaBundle] Added alternative method to PDFImageFilter

--- src/Enhavo/Bundle/MediaBundle/Filter/Filter/PDFImageFilter.php
+++ src/Enhavo/Bundle/MediaBundle/Filter/Filter/PDFImageFilter.php
@@ -8,23 +8,49 @@
 
 namespace Enhavo\Bundle\MediaBundle\Filter\Filter;
 
+use Enhavo\Bundle\MediaBundle\Content\ContentInterface;
 use Enhavo\Bundle\MediaBundle\Exception\FilterException;
 use Enhavo\Bundle\MediaBundle\Filter\AbstractFilter;
+use Enhavo\Bundle\MediaBundle\Model\FileInterface;
 use Enhavo\Bundle\MediaBundle\Model\FilterSetting;
+use Enhavo\Bundle\MediaBundle\Model\FormatInterface;
+use Symfony\Component\Process\Process;
 
 class PDFImageFilter extends AbstractFilter
 {
+    const FILTER_SETTING_GHOSTSCRIPT = 'ghostscript';
+    const FILTER_SETTING_IMAGICK = 'imagick';
+
+    /**
+     * @param ContentInterface|FileInterface|FormatInterface $file
+     * @param FilterSetting $setting
+     * @throws FilterException
+     */
     public function apply($file, FilterSetting $setting)
     {
         $content = $this->getContent($file);
 
-        if(!class_exists("\Imagick")) {
-            throw new FilterException('To use the PDFImageFilter you need to install Imagick extension for php');
-        }
+        if ($setting->getSetting('method') === self::FILTER_SETTING_GHOSTSCRIPT) {
+            $temporaryFileName = tempnam('/tmp', 'pdfImage');
 
-        $imagick = new \Imagick($content->getFilePath().'[0]');
-        $imagick->setImageFormat('jpg');
-        file_put_contents($content->getFilePath(), $imagick);
+            $process = new Process('gs -dSAFER -dBATCH -sDEVICE=jpeg -dTextAlphaBits=4 -dGraphicsAlphaBits=4 -r72 -sOutputFile=' . $temporaryFileName . ' ' . $content->getFilePath());
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new FilterException('PDFImageFilter with method "' . self::FILTER_SETTING_GHOSTSCRIPT . '" failed. Possible reasons could be broken pdf format or missing Ghostscript executable.');
+            }
+
+            copy($temporaryFileName, $content->getFilePath());
+            unlink($temporaryFileName);
+        } else {
+            if(!class_exists("\Imagick")) {
+                throw new FilterException('To use the PDFImageFilter with method "' . self::FILTER_SETTING_IMAGICK . '" (default) you need to install Imagick extension for php.');
+            }
+
+            $imagick = new \Imagick($content->getFilePath().'[0]');
+            $imagick->setImageFormat('jpg');
+            file_put_contents($content->getFilePath(), $imagick);
+        }
 
         $this->setExtension($file, 'jpg');
         $this->setMimeType($file, 'image/jpeg');
