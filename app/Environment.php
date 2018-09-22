eb0907ab084e752141a7d0a0400b906be4b89db1
Gerhard Seidel <gseidel.message@googlemail.com>
2018-09-22 14:06:13 +0200

    add symfony dot env

--- app/Environment.php
+++ app/Environment.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Dotenv\Dotenv;
 
 class Environment
 {
@@ -8,47 +8,32 @@ class Environment
     const DEV = 'dev';
     const TEST = 'test';
 
-
+    /**
+     * @var string
+     */
     private $environment;
+
+    /**
+     * @var bool
+     */
     private $debug;
 
     public function __construct()
     {
-        $this->setEnvironmentInformation();
-    }
+        $dotEnv = new Dotenv();
+        $dotEnv->load(__DIR__.'/../.env');
 
-    protected function setEnvironmentInformation()
-    {
-
-        if(in_array(getenv('ENHAVO_DEBUG'), [self::PROD, self::DEV, self::TEST])) {
-            $this->debug = getenv('ENHAVO_DEBUG');
+        if(getenv('DEBUG')) {
+            $this->debug = getenv('DEBUG');
         } else {
-            $config = Yaml::parse($this->getParametersYMLFilePath());
-            if (isset($config['parameters']['debug'])) {
-                $this->debug = (bool)$config['parameters']['debug'];
-            } else {
-                $this->debug = true;
-            }
+            $this->debug = true;
         }
 
-        if(in_array(getenv('ENHAVO_ENV'), [self::PROD, self::DEV, self::TEST])) {
-            $this->environment = getenv('ENHAVO_ENV');
+        if(in_array(getenv('ENVIRONMENT'), [self::PROD, self::DEV, self::TEST])) {
+            $this->environment = getenv('ENVIRONMENT');
         } else {
-            if(isset($config['parameters']['environment'])) {
-                $this->environment = $config['parameters']['environment'];
-            } else {
-                $this->environment = 'dev';
-            }
+            $this->environment = self::DEV;
         }
-    }
-
-    protected function getParametersYMLFilePath()
-    {
-        $file = __DIR__.'/config/parameters.yml';
-        if(!file_exists($file)) {
-            throw new \RuntimeException('parameters.yml not exists, please add configurition to this file before go on');
-        }
-        return $file;
     }
 
     public function getEnvironment()
