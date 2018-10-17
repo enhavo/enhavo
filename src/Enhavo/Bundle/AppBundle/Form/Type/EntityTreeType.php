4da8fb5590e7870936f2e672f6d93af871a0b970
Gerhard Seidel <gseidel.message@googlemail.com>
2018-10-17 21:51:21 +0200

    add EntityTreeType

---
+++ src/Enhavo/Bundle/AppBundle/Form/Type/EntityTreeType.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gseidel
+ * Date: 16.10.18
+ * Time: 23:45
+ */
+
+namespace Enhavo\Bundle\AppBundle\Form\Type;
+
+use Enhavo\Bundle\AppBundle\Form\Helper\EntityTreeChoiceBuilder;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EntityTreeType extends AbstractType
+{
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $choices = $view->vars['choices'];
+        $builder = new EntityTreeChoiceBuilder($options['parent_property']);
+        $builder->build($choices);
+        $view->vars['choice_tree_builder'] = $builder;
+        $view->vars['children_container_class'] = $options['children_container_class'];
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        $builder = $view->vars['choice_tree_builder'];
+        if($builder instanceof EntityTreeChoiceBuilder) {
+            $builder->map($view);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'parent_property' => 'parent',
+            'children_container_class' => 'entity-tree-children',
+        ]);
+    }
+
+    public function getName()
+    {
+        return 'entity_tree';
+    }
+
+    public function getParent()
+    {
+        return EntityType::class;
+    }
+}
