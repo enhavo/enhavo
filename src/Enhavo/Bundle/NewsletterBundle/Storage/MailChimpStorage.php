4f809972788117dc9735a5241e30534695d69490
Philipp Sester <philipp.sester@xq-web.de>
2018-12-20 15:16:52 +0100

    add mailchimp storage

---
+++ src/Enhavo/Bundle/NewsletterBundle/Storage/MailChimpStorage.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: philippsester
+ * Date: 17.12.18
+ * Time: 12:21
+ */
+
+namespace Enhavo\Bundle\NewsletterBundle\Storage;
+
+use Bundle\MarketplaceBundle\Entity\Grid\StoreItem;
+use Enhavo\Bundle\NewsletterBundle\CleverReach\Client;
+use Enhavo\Bundle\NewsletterBundle\Entity\Group;
+use Enhavo\Bundle\NewsletterBundle\Exception\NoGroupException;
+use Enhavo\Bundle\NewsletterBundle\Group\GroupManager;
+use Enhavo\Bundle\NewsletterBundle\Model\SubscriberInterface;
+use Enhavo\Bundle\NewsletterBundle\Twig\SubscribeFormRenderer;
+
+class MailChimpStorage implements StorageInterface
+{
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    protected $guzzleClient;
+
+    /**
+     * @var String
+     */
+    protected $apiKey;
+
+    /**
+     * @var bool|string
+     */
+    protected $dataCenter;
+
+    /**
+     * @var string[]
+     */
+    protected $mapping;
+
+    public function __construct($credentials, $mapping)
+    {
+        $this->apiKey = $credentials['api_key'];
+        $this->dataCenter = substr($this->apiKey, strpos($this->apiKey, '-') + 1);
+        $this->mapping = $mapping;
+        $this->guzzleClient = new \GuzzleHttp\Client([
+            'base_uri' => $baseUri = 'https://' . $this->dataCenter . '.api.mailchimp.com/3.0/lists/',
+        ]);
+    }
+
+    public function saveSubscriber(SubscriberInterface $subscriber)
+    {
+        if (count($subscriber->getGroups()) === 0) {
+            throw new NoGroupException('no groups given');
+        }
+
+        /** @var Group $group */
+        foreach($subscriber->getGroups() as $group) {
+
+            $response = $this->guzzleClient->request('POST', $this->mapping[$group->getCode()] . '/members', [
+                'auth' => [
+                    'user',
+                    $this->apiKey,
+                ],
+                'headers' => [
+                    'content-type' => 'application/json'
+                ],
+                'body' => json_encode([
+                    'email_address' => $subscriber->getEmail(),
+                    'status' => 'subscribed',
+                ]),
+            ]);
+        }
+    }
+
+    public function exists(SubscriberInterface $subscriber)
+    {
+        if (count($subscriber->getGroups()) === 0) {
+            return false;
+        }
+
+        /** @var Group $group */
+        foreach ($subscriber->getGroups() as $group) {
+            $memberID = md5(strtolower($subscriber->getEmail()));
+
+            $response = $this->guzzleClient->request('GET', $this->mapping[$group->getCode()] . '/members/' . $memberID, [
+                'http_errors' => false,
+                'auth' => [
+                    'user',
+                    $this->apiKey,
+                ]
+            ]);
+            if($response->getStatusCode() == 404) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function getType()
+    {
+        return 'mailchimp';
+    }
+}
