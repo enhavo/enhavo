8940fc4b6bdeb5873f789a45c3dfdcf410ed4ce4
Gerhard Seidel <gseidel.message@googlemail.com>
2019-01-12 00:44:24 +0100

    add app test view

--- src/Enhavo/Bundle/AppBundle/Controller/AppController.php
+++ src/Enhavo/Bundle/AppBundle/Controller/AppController.php
@@ -8,45 +8,15 @@
 
 namespace Enhavo\Bundle\AppBundle\Controller;
 
-use Enhavo\Bundle\AppBundle\Viewer\ViewFactory;
-use FOS\RestBundle\View\ViewHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AppController extends AbstractController
 {
-    /**
-     * @var ViewHandlerInterface
-     */
-    private $viewHandler;
-
-    /**
-     * @var ViewFactory
-     */
-    private $viewFactory;
-
-    public function __construct(
-        ViewFactory $viewFactory,
-        ViewHandlerInterface $viewHandler
-    ) {
-        $this->viewFactory = $viewFactory;
-        $this->viewHandler = $viewHandler;
-    }
-
     public function indexAction(Request $request)
     {
-        $view = $this->viewFactory->create('app', [
-            'request' => $request
-        ]);
-        return $this->viewHandler->handle($view);
-    }
+        return $this->render('EnhavoAppBundle:App:index.html.twig', [
 
-    public function showAction($contentDocument, Request $request)
-    {
-        $view = $this->viewFactory->create('base', [
-            'request' => $request,
-            'resource' => $contentDocument
         ]);
-        return $this->viewHandler->handle($view);
     }
 }
