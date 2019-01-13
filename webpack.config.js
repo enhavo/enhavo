88f58b6a548276f3519541027a9b666f0e1bceee
Gerhard Seidel <gseidel.message@googlemail.com>
2019-01-13 19:37:52 +0100

    update packages and config to use vue with ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,52 @@
-
 var Encore = require('@symfony/webpack-encore');
-
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 Encore
   .setOutputPath('public/build/')
   .setPublicPath('/build')
   .addEntry('enhavo/app', './assets/app')
-  .enableTypeScriptLoader()
-  .enableForkedTypeScriptTypesChecking()
   .autoProvidejQuery()
 ;
 
 var config = Encore.getWebpackConfig();
+
+config.resolve = {
+  extensions: [".js", ".ts", ".tsx"]
+};
+
+config.module = {
+  rules: [
+    {
+      test: /\.vue$/,
+      loader: "vue-loader"
+    },
+    {
+      test: /\.tsx?$/,
+      loader: "ts-loader",
+      options: {
+        allowTsInNodeModules: true,
+        transpileOnly: true,
+        appendTsSuffixTo: [/\.vue$/]
+      }
+    },
+    {
+      test: /\.scss$/,
+      use: [
+        'vue-style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'vue-style-loader',
+        'css-loader'
+      ]
+    }
+  ]
+};
+
+config.plugins.push(new VueLoaderPlugin());
 
 module.exports = config;
