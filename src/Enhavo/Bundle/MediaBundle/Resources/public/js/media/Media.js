78eaa2e1b3e15e970fb3403d7432611f5818d1f5
Fabian Liebl <fabian.liebl@xq-web.de>
2019-05-02 11:30:59 +0200

    Fix for media form javascript displaying default icon on unknown mimeType

--- src/Enhavo/Bundle/MediaBundle/Resources/public/js/media/Media.js
+++ src/Enhavo/Bundle/MediaBundle/Resources/public/js/media/Media.js
@@ -428,7 +428,7 @@ define(["require", "exports", "jquery", "blueimp-file-upload", "jquery-ui"], fun
                     this.$element.find('[data-media-thumb-icon]').addClass('icon-file-code');
                     break;
                 default:
-                    this.$element.find('[data-file-icon]').addClass('icon-doc');
+                    this.$element.find('[data-media-thumb-icon]').addClass('icon-doc');
             }
         };
         MediaItem.prototype.getThumbUrl = function () {
