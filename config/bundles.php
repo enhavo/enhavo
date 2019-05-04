fdde77a8ea982cf2eb261afad88a718a1c4659e2
Gerhard Seidel <gseidel.message@googlemail.com>
2019-05-05 01:37:19 +0200

    cleanup theme bundle

--- config/bundles.php
+++ config/bundles.php
@@ -47,6 +47,7 @@ return [
     Enhavo\Bundle\RoutingBundle\EnhavoRoutingBundle::class => ['all' => true],
     Enhavo\Bundle\NavigationBundle\EnhavoNavigationBundle::class => ['all' => true],
     Enhavo\Bundle\ProjectBundle\EnhavoProjectBundle::class => ['all' => true],
+    Enhavo\Bundle\ThemeBundle\EnhavoThemeBundle::class => ['all' => true],
     Enhavo\Bundle\TranslationBundle\EnhavoTranslationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Enhavo\Bundle\ShopBundle\EnhavoShopBundle::class => ['all' => true],
