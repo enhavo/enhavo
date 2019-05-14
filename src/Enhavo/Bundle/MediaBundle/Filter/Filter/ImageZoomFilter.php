454035a08af1bf983e1333b4e4ed6ab518d19753
Gerhard Seidel <gseidel.message@googlemail.com>
2019-05-14 22:29:03 +0200

    add image zoom filter

---
+++ src/Enhavo/Bundle/MediaBundle/Filter/Filter/ImageZoomFilter.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gseidel
+ * Date: 11.11.17
+ * Time: 12:09
+ */
+
+namespace Enhavo\Bundle\MediaBundle\Filter\Filter;
+
+use Enhavo\Bundle\MediaBundle\Filter\AbstractFilter;
+use Enhavo\Bundle\MediaBundle\Model\FilterSetting;
+use Imagine\Exception\RuntimeException;
+use Imagine\Gd\Imagine;
+use Imagine\Image\Box;
+
+class ImageZoomFilter extends AbstractFilter
+{
+    public function apply($file, FilterSetting $setting)
+    {
+        $content = $this->getContent($file);
+
+        try {
+            $imagine = new Imagine();
+            $imagine = $imagine->load($content->getContent());
+            $factor = $setting->getSetting('zoom', 1);
+
+            $size = $imagine->getSize();
+            $height = intval(round($size->getHeight()*$factor));
+            $width =  intval(round($size->getWidth()*$factor));
+
+            $imagine->resize(new Box($width, $height));
+
+            $imagine->save($content->getFilePath(), [
+                'format' => $this->getImageFormat($content)
+            ]);
+        } catch (RuntimeException $e) {
+            // if image cant be created we make an empty file
+            file_put_contents($content->getFilePath(), '');
+        }
+    }
+
+    public function getType()
+    {
+        return 'image_zoom';
+    }
+}
