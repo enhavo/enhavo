1f4ffc1f65e5aab4fb424ff69e77287986fcc32d
Gerhard Seidel <gseidel.message@googlemail.com>
2019-05-22 00:59:01 +0200

    execute migrations on docker

--- docker/startup/install.bash
+++ docker/startup/install.bash
@@ -2,11 +2,6 @@
 
 echo "Install mysql"
 /var/www/bin/console doctrine:database:create
-/var/www/bin/console doctrine:schema:update --force
+/var/www/bin/console doctrine:migration:migrate -n
 /var/www/bin/console fos:user:create admin@enhavo.com admin@enhavo.com admin
 /var/www/bin/console fos:user:promote admin@enhavo.com ROLE_SUPER_ADMIN
-echo "Install assets"
-cd /var/www/
-yarn install
-yarn encore prod
-bin/console fos:js-routing:dump --format=json --target=public/js/fos_js_routes.json
