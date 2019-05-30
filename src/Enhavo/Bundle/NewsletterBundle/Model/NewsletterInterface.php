5174bdd43a0be135a3f945a5de71daa59a89aee9
Gerhard Seidel <gseidel.message@googlemail.com>
2019-05-30 11:16:11 +0200

    use interfaces for newsletter entities

---
+++ src/Enhavo/Bundle/NewsletterBundle/Model/NewsletterInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gseidel
+ * Date: 2019-05-29
+ * Time: 18:19
+ */
+
+namespace Enhavo\Bundle\NewsletterBundle\Model;
+
+
+interface NewsletterInterface
+{
+
+}
