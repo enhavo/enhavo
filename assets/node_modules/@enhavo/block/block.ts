cfbccbe78a21d8094fb6e95252a954b0f7618f78
Nico Pakai <nicolas.pakai@xq-web.de>
2019-07-03 18:03:10 +0200

    styled pagination

--- assets/node_modules/@enhavo/block/block.ts
+++ assets/node_modules/@enhavo/block/block.ts
@@ -19,6 +19,23 @@ export default class Block
                 fade: true,
                 dots:true
             });
+
+            window.onload = function(){
+                var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
+                $('.cdp_i').on('click', function(){
+                    var go = $(this).attr('href').replace('#!', '');
+                    if (go === '+1') {
+                        paginationPage++;
+                    } else if (go === '-1') {
+                        paginationPage--;
+                    }else{
+                        paginationPage = parseInt(go, 10);
+                    }
+                    $('.cdp').attr('actpage', paginationPage);
+                });
+            };
+
+
         });
     }
 }
