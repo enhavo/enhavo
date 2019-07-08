66a14b1b80bdf5c0e1d01fa924f0fd254aab2705
Gerhard Seidel <gseidel.message@googlemail.com>
2019-07-08 22:44:32 +0200

    add template configuration

---
+++ src/Enhavo/Bundle/AppBundle/Exception/TemplateNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * NoViewerFoundException.php
+ *
+ * @since 29/05/15
+ * @author gseidel
+ */
+
+namespace Enhavo\Bundle\AppBundle\Exception;
+
+class TemplateNotFoundException extends \Exception {
+
+}
