29baca8091ce3caaf9f85bd61b880aed4df57082
Nico Pakai <nicolas.pakai@xq-web.de>
2019-07-08 13:24:19 +0200

    responsive calendar styles

--- assets/node_modules/@enhavo/block/block.ts
+++ assets/node_modules/@enhavo/block/block.ts
@@ -14,7 +14,18 @@ export default class Block
         document.addEventListener('DOMContentLoaded', function() {
             let calendarEl = document.getElementById('calendar');
             let calendar = new Calendar(calendarEl, {
-                plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ]
+                plugins: [ dayGridPlugin, timeGridPlugin, listPlugin ],
+                aspectRatio: 1.5
+            });
+
+            let event = $('[data-event-container]').find('[data-event]');
+            event.each(function() {
+                let eventTitle = $(this).data('appointment-title');
+                let eventStart = $(this).data('appointment-start');
+                let eventEnd = $(this).data('appointment-end');
+                let eventUrl = $(this).data('appointment-url');
+
+                calendar.addEvent({ title: eventTitle, start: eventStart, end: eventEnd, url: eventUrl});
             });
             calendar.render();
         });
