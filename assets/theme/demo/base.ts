684ded2b28e804bf4f2da77edb4d5abb0eeb49fe
Gerhard Seidel <gseidel.message@googlemail.com>
2019-07-15 00:09:26 +0200

    fix theme loading

--- assets/theme/demo/base.ts
+++ assets/theme/demo/base.ts
@@ -1,8 +1,11 @@
+import "@enhavo/theme/init"
 import "@enhavo/block/init"
 import "@enhavo/slider/init"
 import "@enhavo/shop/init"
 import "@enhavo/article/init"
 import "@enhavo/calendar/init"
+import "./styles/style.scss"
+
 
 import Theme from "./lib/Theme";
 import * as $ from "jquery";
