6f0f8faeaaedd1edd19fb787fa1a79b7c2dfa504
Gerhard Seidel <gseidel.message@googlemail.com>
2019-07-17 00:03:58 +0200

    add sortable to entity type

--- assets/node_modules/@enhavo/form/Type/SelectType.ts
+++ assets/node_modules/@enhavo/form/Type/SelectType.ts
@@ -4,6 +4,7 @@ import 'select2'
 import 'select2/select2.css'
 import 'select2/select2_locale_de.js'
 import 'select2/select2_locale_en.js.template'
+import * as Sortable from "sortablejs";
 
 export default class SelectType extends FormType
 {
@@ -13,6 +14,35 @@ export default class SelectType extends FormType
         if ($.fn.select2.locales.hasOwnProperty(locale)) {
             $.extend($.fn.select2.defaults, $.fn.select2.locales[locale]);
         }
+
+        let data = this.$element.data('select2-options');
         this.$element.select2();
+        let $list = this.$element.parent().find('ul.select2-choices');
+
+        if($list.get(0) && data && data.sortable && data.multiple) {
+            let listElement = <HTMLElement>$list.get(0);
+            new Sortable(listElement, {
+                draggable: ".select2-search-choice",
+                animation: 150,
+                onUpdate: () => {
+                    let data = <Array<any>>this.$element.select2('data');
+                    let list: Array<string> = [];
+                    for(let element of data) {
+                        list.push(element.id);
+                    }
+                    this.$element.children().each((index, element) => {
+                        let value = $(element).val();
+                        let newIndex = list.indexOf(value);
+                        let beforeElement = this.$element.children().get(newIndex);
+                        if(beforeElement != element) {
+                            $(beforeElement).after(element);
+                        }
+                    });
+                },
+                onEnd: () => {
+                    $list.find('.select2-search-field').appendTo($list.get(0));
+                },
+            });
+        }
     }
 }
