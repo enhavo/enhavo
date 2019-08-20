6a71a5c08bbd0de2386831b314c1ea3a6c717b7d
Philipp Sester <philipp.sester@xq-web.de>
2019-08-20 14:10:35 +0200

    add group instead of subscriber to newsletter-main-menu; select subscriber out of group-table-view

---
+++ src/Enhavo/Bundle/NewsletterBundle/Factory/SubscriberFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Enhavo\Bundle\NewsletterBundle\Factory;
+
+use Enhavo\Bundle\AppBundle\Factory\Factory;
+use Enhavo\Bundle\NewsletterBundle\Entity\Group;
+use Enhavo\Bundle\SliderBundle\Model\SliderInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+
+class SubscriberFactory extends Factory
+{
+    /**
+     * @var RepositoryInterface
+     */
+    private $groupRepository;
+
+    public function __construct(string $className, RepositoryInterface $groupRepository)
+    {
+        parent::__construct($className);
+        $this->groupRepository = $groupRepository;
+    }
+
+    public function createWithGroupId($groupId)
+    {
+        /** @var SliderInterface $slider */
+        $group = $this->groupRepository->find($groupId);
+
+        /** @var Group $group */
+        $subscriber = $this->createNew();
+        $subscriber->addGroup($group);
+        return $subscriber;
+    }
+}
