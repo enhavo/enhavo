2986c82b8994a321875a4449648f8c4cc3bc2f95
Gerhard Seidel <gseidel.message@googlemail.com>
2019-08-28 01:05:02 +0200

    show already sent message and simplify newsletter api

---
+++ src/Enhavo/Bundle/NewsletterBundle/Exception/SendException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gseidel
+ * Date: 2019-08-28
+ * Time: 00:38
+ */
+
+namespace Enhavo\Bundle\NewsletterBundle\Exception;
+
+
+class SendException extends \Exception
+{
+
+}
