98dbb59ce3766a614586aba83def722ebeb891f5
Gerhard Seidel <gseidel.message@googlemail.com>
2019-09-15 20:56:00 +0200

    locale default generator

---
+++ src/Enhavo/Bundle/TranslationBundle/Locale/LocaleResolver.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * LocalResolver.php
+ *
+ * @since 27/11/16
+ * @author gseidel
+ */
+
+namespace Enhavo\Bundle\TranslationBundle\Locale;
+
+use Enhavo\Bundle\AppBundle\Locale\LocaleResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\RequestStack;
+use League\Uri\Components\HierarchicalPath;
+
+class LocaleResolver implements LocaleResolverInterface
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @var string[]
+     */
+    private $locales;
+
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
+    public function __construct(RequestStack $requestStack, $locales, $defaultLocale)
+    {
+        $this->locales = $locales;
+        $this->defaultLocale = $defaultLocale;
+        $this->requestStack = $requestStack;
+    }
+
+    public function resolve()
+    {
+        if($this->locale) {
+            return $this->locale;
+        }
+
+        $this->resolveLocale();
+        return $this->locale;
+    }
+
+    private function resolveLocale()
+    {
+        $this->locale = $this->defaultLocale;
+        $request = $this->requestStack->getMasterRequest();
+        if($request !== null) {
+            $locale = $request->attributes->get('_locale');
+            if($locale) {
+                $this->locale = $locale;
+            }
+        }
+    }
+}
