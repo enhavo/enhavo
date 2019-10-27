b1e43840e4643280e1262f01628eccc07c27f82a
Gerhard Seidel <gseidel.message@googlemail.com>
2019-10-27 21:35:59 +0100

    add column comment state

---
+++ assets/node_modules/@enhavo/app/Grid/Column/Model/StateColumn.ts
@@ -0,0 +1,7 @@
+import AbstractColumn from "@enhavo/app/Grid/Column/Model/AbstractColumn";
+
+export default class StateColumn extends AbstractColumn
+{
+    property: string;
+    wrap: boolean;
+}
