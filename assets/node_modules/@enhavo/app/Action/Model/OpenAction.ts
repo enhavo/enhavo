b5af41dd88326efe3fd8b16cabb40bd34dbe13a0
Gerhard Seidel <gseidel.message@googlemail.com>
2019-10-27 21:36:35 +0100

    fix open comments action

--- assets/node_modules/@enhavo/app/Action/Model/OpenAction.ts
+++ assets/node_modules/@enhavo/app/Action/Model/OpenAction.ts
@@ -4,11 +4,16 @@ export default class OpenAction extends AbstractAction
 {
     public url: string;
     public target: string;
+    public key: string;
 
     execute(): void
     {
         if(this.target == '_view') {
-            this.application.getView().open(this.url);
+            if(this.key) {
+                this.application.getView().open(this.url, this.key);
+            } else {
+                this.application.getView().open(this.url);
+            }
             return;
         }
         window.open(this.url, this.target);
