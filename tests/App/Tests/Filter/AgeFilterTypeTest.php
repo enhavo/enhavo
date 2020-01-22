ed4885942a2d7bf5b08145280bcffd939b5bbdc4
Gerhard Seidel <gseidel.message@googlemail.com>
2020-01-22 08:08:33 +0800

    move tests into App namespace

---
+++ tests/App/Tests/Filter/AgeFilterTypeTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gseidel
+ * Date: 2020-01-21
+ * Time: 14:17
+ */
+
+namespace App\Tests\Filter;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AgeFilterTypeTest extends WebTestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
