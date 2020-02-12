531a7bb6a2d495f94313537ae52e9f9e9da9e61c
Gerhard Seidel <gseidel.message@googlemail.com>
2020-02-12 14:32:25 +0800

    add headline test

--- src/Enhavo/Bundle/AppBundle/Tests/Twig/FormatExtenstionTest.php
+++ src/Enhavo/Bundle/AppBundle/Tests/Twig/FormatExtenstionTest.php
@@ -21,4 +21,39 @@ class FormatExtensionTest extends TestCase
         $this->assertEquals('<p></p><p>Test</p>', $extension->sanitizeHtml('<p><iframe>Hello</iframe></p><p>Test</p>'));
         $this->assertEquals('<p>Test</p>', $extension->sanitizeHtml('Test'));
     }
+
+    public function testFormatHeadline()
+    {
+        $extension = new FormatExtension();
+
+        $this->assertEquals(
+            '<h1>Test</h1>',
+            $extension->formatHeadline('<h1>Test</h1>'),
+            'No formation should be added'
+        );
+
+        $this->assertEquals(
+            '<div>Test</div>',
+            $extension->formatHeadline('Test'),
+            'Block formation should be added'
+        );
+
+        $this->assertEquals(
+            '<h1 class="my-class">Test</h1>',
+            $extension->formatHeadline('<h1>Test</h1>', 'my-class'),
+            'Additional class should appear'
+        );
+
+        $this->assertEquals(
+            '<h1 class="my-class" data-additional="value">Test</h1>',
+            $extension->formatHeadline('<h1>Test</h1>', 'my-class', ['data-additional' => 'value']),
+            'Additional attribute should appear'
+        );
+
+        $this->assertEquals(
+            '<h1 class="my-class" data-additional="{&quot;key&quot;:&quot;value&quot;}">Test</h1>',
+            $extension->formatHeadline('<h1>Test</h1>', 'my-class', ['data-additional' => ['key' => 'value']]),
+            'Array should be json encoded'
+        );
+    }
 }
