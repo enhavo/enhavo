dbe58414fa439e0d1e8efeb34bacfe2c560dbb29
indivisual <ennyennson@gmail.com>
2020-04-29 10:51:46 +0200

    [FormBundle] Fix count for expanded true #889 + Missing Migration (#891)
    * [FormBundle] Fix count for expanded true #889
    
    * added missing migration

--- assets/node_modules/@enhavo/form/Type/CheckboxType.ts
+++ assets/node_modules/@enhavo/form/Type/CheckboxType.ts
@@ -5,9 +5,20 @@ export default class CheckboxType extends FormType
 {
     protected init()
     {
-        this.$element.iCheck({
+        let iCheck = this.$element.iCheck({
             checkboxClass: 'icheckbox',
             radioClass: 'icheckbox'
-        })
+        });
+
+        let $formRow = this.$element.parents('[data-form-row]');
+        let $count = $formRow.find('[data-selected-count]');
+
+        if ($count.length) { // if there is a data-selected-count element, then multiple must have been true
+            iCheck.on('ifChanged', (event: any) => {
+                let checked = $formRow.find('input:checked');
+                let count = checked.length;
+                $count.text('(' + count + ')');
+            });
+        }
     }
 }
