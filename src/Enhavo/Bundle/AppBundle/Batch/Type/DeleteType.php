3a9a24b8090d924fb6e551e7257d2a4f02236b0e
blutze <account@bennson.de>
2020-04-29 12:04:28 +0200

    add missing dispatches of pre and post delete resource

--- src/Enhavo/Bundle/AppBundle/Batch/Type/DeleteType.php
+++ src/Enhavo/Bundle/AppBundle/Batch/Type/DeleteType.php
@@ -3,6 +3,9 @@
 namespace Enhavo\Bundle\AppBundle\Batch\Type;
 
 use Enhavo\Bundle\AppBundle\Batch\AbstractBatchType;
+use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -13,6 +16,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class DeleteType extends AbstractBatchType
 {
+
+    /** @var EventDispatcherInterface */
+    protected $eventDispatcher;
+
+    /**
+     * DeleteType constructor.
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(EventDispatcherInterface $eventDispatcher)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+
     /**
      * @inheritdoc
      */
@@ -20,7 +37,9 @@ class DeleteType extends AbstractBatchType
     {
         $em = $this->container->get('doctrine.orm.entity_manager');
         foreach($resources as $resource) {
+            $this->eventDispatcher->dispatch(sprintf('enhavo_app.pre_%s', ResourceActions::DELETE), new ResourceControllerEvent($resource));
             $em->remove($resource);
+            $this->eventDispatcher->dispatch(sprintf('enhavo_app.post_%s', ResourceActions::DELETE), new ResourceControllerEvent($resource));
         }
         $em->flush();
     }
