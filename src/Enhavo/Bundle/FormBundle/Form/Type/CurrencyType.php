7f19591ab31c231500df27b35b2f7a8f912ab0a2
Benjamin Lutze <account@bennson.de>
2020-05-06 06:16:54 +0200

    unified currency transformation

--- src/Enhavo/Bundle/FormBundle/Form/Type/CurrencyType.php
+++ src/Enhavo/Bundle/FormBundle/Form/Type/CurrencyType.php
@@ -8,6 +8,7 @@
 
 namespace Enhavo\Bundle\FormBundle\Form\Type;
 
+use Enhavo\Bundle\FormBundle\Form\Transformer\CurrencyTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -18,42 +19,7 @@ class CurrencyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->addModelTransformer(new CallbackTransformer(
-                function ($currencyAsInt) {
-                    //int -> text
-                    $string = (string)$currencyAsInt;
-                    $string = str_pad($string, 3, '0', STR_PAD_LEFT);
-                    $length = strlen($string);
-                    $right = substr($string, $length - 2, 2 );
-                    $left = substr($string, 0, $length - 2);
-                    $value = $left.','.$right;
-                    return $value;
-                },
-                function ($currencyAsString) {
-                    //text -> int
-                    $string = $currencyAsString;
-                    $string = str_replace('.', '', $string);
-
-                    $parts = explode(',', $string);
-                    $right = 0;
-                    if(count($parts) > 1) {
-                        $right = array_pop($parts);
-                        $right = substr($right, 0, 2);
-                        $right = str_pad($right, 2, '0');
-                        $right = intval($right);
-                    }
-
-                    $left = implode($parts);
-                    $left = intval($left);
-
-                    $value = $right;
-                    if($left > 0) {
-                        $value = $left*100 + $value;
-                    }
-
-                    return $value;
-                }
-            ))
+            ->addModelTransformer(new CurrencyTransformer())
         ;
     }
 
