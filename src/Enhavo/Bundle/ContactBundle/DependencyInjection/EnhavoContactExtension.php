c28975eefb5b907082f51d7b17af8442c5f72421
Benjamin Lutze <account@bennson.de>
2020-05-07 07:25:29 +0200

    ContactBundle yml2yaml

--- src/Enhavo/Bundle/ContactBundle/DependencyInjection/EnhavoContactExtension.php
+++ src/Enhavo/Bundle/ContactBundle/DependencyInjection/EnhavoContactExtension.php
@@ -43,6 +43,6 @@ class EnhavoContactExtension extends Extension
         }
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
