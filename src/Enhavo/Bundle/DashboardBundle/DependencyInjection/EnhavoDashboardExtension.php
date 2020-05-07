6eb2536eb2797e6555e5d0bd01806d91b6a93372
Benjamin Lutze <account@bennson.de>
2020-05-07 07:29:51 +0200

    DashboardBundle yml2yaml

--- src/Enhavo/Bundle/DashboardBundle/DependencyInjection/EnhavoDashboardExtension.php
+++ src/Enhavo/Bundle/DashboardBundle/DependencyInjection/EnhavoDashboardExtension.php
@@ -23,6 +23,6 @@ class EnhavoDashboardExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
     }
 }
