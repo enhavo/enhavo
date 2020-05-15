d751bf2fac9122675d308c4484330606775ba269
FabianLiebl <fabian.liebl@xq-web.de>
2020-05-15 04:21:53 +0200

    [MediaBundle] Added EventListener to delete files together with their associated file objects (#919)

--- src/Enhavo/Bundle/MediaBundle/EventListener/DoctrineSubscriber.php
+++ src/Enhavo/Bundle/MediaBundle/EventListener/DoctrineSubscriber.php
@@ -8,9 +8,10 @@
 
 namespace Enhavo\Bundle\MediaBundle\EventListener;
 
-
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Enhavo\Bundle\MediaBundle\Media\FormatManager;
 use Enhavo\Bundle\MediaBundle\Model\FileInterface;
 use Enhavo\Bundle\MediaBundle\Model\FormatInterface;
 use Enhavo\Bundle\MediaBundle\Storage\StorageInterface;
@@ -22,17 +23,24 @@ class DoctrineSubscriber implements EventSubscriber
      */
     private $storage;
 
-    public function __construct(StorageInterface $storage)
+    /**
+     * @var FormatManager
+     */
+    private $formatManager;
+
+    public function __construct(StorageInterface $storage, FormatManager $formatManager)
     {
         $this->storage = $storage;
+        $this->formatManager = $formatManager;
     }
 
     public function getSubscribedEvents()
     {
         return array(
-            'postLoad',
-            'postUpdate',
-            'postPersist',
+            Events::postLoad,
+            Events::postUpdate,
+            Events::postPersist,
+            Events::preRemove,
         );
     }
 
@@ -48,7 +56,7 @@ class DoctrineSubscriber implements EventSubscriber
     {
         $object = $args->getObject();
         if($object instanceof FileInterface) {
-            $this->storage->saveFile($object);;
+            $this->storage->saveFile($object);
         }
     }
 
@@ -59,6 +67,17 @@ class DoctrineSubscriber implements EventSubscriber
             $this->storage->applyContent($object);
         } elseif($object instanceof FormatInterface) {
             $this->storage->applyContent($object);
+        }
+    }
+
+    public function preRemove(LifecycleEventArgs $args)
+    {
+        $object = $args->getObject();
+        if($object instanceof FileInterface) {
+            $this->storage->deleteFile($object);
+            // Delete on files doesn't use a doctrine cascade to formats, but an SQL cascade instead. This means that formats will be deleted without triggering a doctrine event.
+            // So to clean up files associated with formats, we must delete them in the doctrine event for the file instead of the format.
+            $this->formatManager->deleteFormats($object);
         }
     }
 }
