daed4f7d906d89d529ac1e7b863ade1875920da4
Benjamin Lutze <account@bennson.de>
2020-07-21 11:01:53 +0200

    fix missing column property when sorting is enabled

--- src/Enhavo/Bundle/AppBundle/Column/Type/DateTimeType.php
+++ src/Enhavo/Bundle/AppBundle/Column/Type/DateTimeType.php
@@ -27,7 +27,7 @@ class DateTimeType extends AbstractColumnType
         $data = parent::createColumnViewData($options);
 
         $data = array_merge($data, [
-
+            'property' => $options['property']
         ]);
 
         return $data;
