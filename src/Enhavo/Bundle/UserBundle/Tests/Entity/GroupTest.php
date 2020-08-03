4f0cf178b66b670b6ae786bd9e9547b12ede4f6f
Benjamin Lutze <account@bennson.de>
2020-08-03 14:15:40 +0200

    user-bundle tests

---
+++ src/Enhavo/Bundle/UserBundle/Tests/Entity/GroupTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Enhavo\Bundle\UserBundle\Tests\Entity;
+
+
+use Enhavo\Bundle\UserBundle\Entity\Group;
+use Enhavo\Bundle\UserBundle\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class GroupTest extends TestCase
+{
+    private function createInstance()
+    {
+        return new Group();
+    }
+
+    public function test()
+    {
+        $group = $this->createInstance();
+        $group->setName('_test');
+        $group->addRole(User::ROLE_SUPER_ADMIN);
+        $this->assertContains(User::ROLE_SUPER_ADMIN, $group->getRoles());
+        $group->removeRole(User::ROLE_SUPER_ADMIN);
+        $this->assertNotContains(User::ROLE_SUPER_ADMIN, $group->getRoles());
+
+        $user = new User();
+        $group->addUser($user);
+        $this->assertContains($user, $group->getUsers());
+        $group->removeUser($user);
+        $this->assertNotContains($user, $group->getUsers());
+
+        $this->assertEquals($group->getName(), (string)$group);
+    }
+}
