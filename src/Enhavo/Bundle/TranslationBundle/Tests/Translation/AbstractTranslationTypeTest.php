08400a91f3527580102f12bb55abced0d39af2d8
Benjamin Lutze <account@bennson.de>
2020-08-11 08:29:31 +0200

    test update

--- src/Enhavo/Bundle/TranslationBundle/Tests/Translation/AbstractTranslationTypeTest.php
+++ src/Enhavo/Bundle/TranslationBundle/Tests/Translation/AbstractTranslationTypeTest.php
@@ -6,36 +6,47 @@ namespace Enhavo\Bundle\TranslationBundle\Tests\Translation;
 use Enhavo\Bundle\TranslationBundle\Translation\AbstractTranslationType;
 use Enhavo\Bundle\TranslationBundle\Translation\TranslationTypeInterface;
 use Enhavo\Bundle\TranslationBundle\Translation\Type\TranslationType;
+use Enhavo\Bundle\TranslationBundle\Translator\TranslatorInterface;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class AbstractTranslationTypeTest extends TestCase
 {
+    private function createDependencies()
+    {
+        $dependencies = new AbstractTranslationTypeTestDependencies();
+        $dependencies->translator = $this->getMockBuilder(TranslatorInterface::class)->getMock();
+        $dependencies->translation = $this->getMockBuilder(TranslationTypeInterface::class)->getMock();
+
+        return $dependencies;
+    }
+
     public function testSetTranslation()
     {
-        $parentMock = $this->getMockBuilder(TranslationTypeInterface::class)->getMock();
-        $parentMock->expects($this->once())->method('setTranslation');
-        $type = new ConcreteTranslationType();
-        $type->setParent($parentMock);
+        $dependencies = $this->createDependencies();
+        $dependencies->translation->expects($this->once())->method('setTranslation');
+        $type = new ConcreteTranslationType($dependencies->translator);
+        $type->setParent($dependencies->translation);
 
         $type->setTranslation([], null, null, 'en', 'value');
     }
 
     public function testGetTranslation()
     {
-        $parentMock = $this->getMockBuilder(TranslationTypeInterface::class)->getMock();
-        $parentMock->expects($this->once())->method('getTranslation');
-        $type = new ConcreteTranslationType();
-        $type->setParent($parentMock);
+        $dependencies = $this->createDependencies();
+        $dependencies->translation->expects($this->once())->method('getTranslation');
+        $type = new ConcreteTranslationType($dependencies->translator);
+        $type->setParent($dependencies->translation);
 
         $type->getTranslation([], null, null, 'en');
     }
 
     public function testGetValidationConstraint()
     {
-        $parentMock = $this->getMockBuilder(TranslationTypeInterface::class)->getMock();
-        $parentMock->expects($this->once())->method('getValidationConstraints');
-        $type = new ConcreteTranslationType();
-        $type->setParent($parentMock);
+        $dependencies = $this->createDependencies();
+        $dependencies->translation->expects($this->once())->method('getValidationConstraints');
+        $type = new ConcreteTranslationType($dependencies->translator);
+        $type->setParent($dependencies->translation);
 
         $type->getValidationConstraints([], null, null, 'en');
     }
@@ -44,6 +55,15 @@ class AbstractTranslationTypeTest extends TestCase
     {
         $this->assertEquals(TranslationType::class, ConcreteTranslationType::getParentType());
     }
+}
+
+class AbstractTranslationTypeTestDependencies
+{
+    /** @var TranslatorInterface|MockObject */
+    public $translator;
+
+    /** @var TranslationTypeInterface|MockObject */
+    public $translation;
 }
 
 class ConcreteTranslationType extends AbstractTranslationType
