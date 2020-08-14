c6533518a4b3a3e71047eadfa638510ad5414d54
Benjamin Lutze <account@bennson.de>
2020-08-14 11:48:50 +0200

    enhance tests

--- src/Enhavo/Bundle/TranslationBundle/Tests/Locale/LocaleResolverTest.php
+++ src/Enhavo/Bundle/TranslationBundle/Tests/Locale/LocaleResolverTest.php
@@ -4,26 +4,28 @@
 namespace Enhavo\Bundle\TranslationBundle\Tests\Locale;
 
 
-use Enhavo\Bundle\TranslationBundle\Locale\LocalePathResolver;
+use Enhavo\Bundle\TranslationBundle\Locale\LocaleResolver;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 
-class LocalePathResolverTest extends TestCase
+class LocaleResolverTest extends TestCase
 {
     private function createDependencies()
     {
-        $dependencies = new LocalePathResolverTestDependencies();
+        $dependencies = new LocaleResolverTestDependencies();
         $dependencies->requestStack = $this->getMockBuilder(RequestStack::class)->getMock();
         $dependencies->request = $this->getMockBuilder(Request::class)->getMock();
+        $dependencies->request->attributes = $this->getMockBuilder(ParameterBag::class)->getMock();
 
         return $dependencies;
     }
 
     private function createInstance($dependencies)
     {
-        $resolver = new LocalePathResolver(
+        $resolver = new LocaleResolver(
             $dependencies->requestStack,
             ['fr', 'es'],
             'fr'
@@ -38,40 +40,43 @@ class LocalePathResolverTest extends TestCase
         $resolver = $this->createInstance($dependencies);
 
         $dependencies->requestStack->method('getMasterRequest')->willReturn($dependencies->request);
-        $dependencies->request->method('getPathInfo')->willReturn('/es/action/id');
+        $dependencies->request->attributes->method('get')->willReturnCallback(function($property) {
+            if ($property === '_locale') {
+                return 'es';
+            }
+        });
+
 
         $this->assertEquals('es', $resolver->resolve());
     }
 
-    public function testResolveFallback()
+    public function testResolveSet()
     {
         $dependencies = $this->createDependencies();
         $resolver = $this->createInstance($dependencies);
 
         $dependencies->requestStack->method('getMasterRequest')->willReturn($dependencies->request);
-        $dependencies->request->method('getPathInfo')->willReturn('/en/action/id');
+        $dependencies->request->attributes->method('get')->willReturnCallback(function($property) {
+            if ($property === '_locale') {
+                return 'es';
+            }
+        });
 
-        $this->assertEquals('fr', $resolver->resolve());
-    }
-
-    public function testResolveRepeat()
-    {
-        $dependencies = $this->createDependencies();
-        $resolver = $this->createInstance($dependencies);
-
-        $dependencies->requestStack->method('getMasterRequest')->willReturn($dependencies->request);
-        $dependencies->request->method('getPathInfo')->willReturn('/es/action/id');
 
         $this->assertEquals('es', $resolver->resolve());
 
         // test if it will not be resolved again
-        $dependencies->request->method('getPathInfo')->willReturn('/en/action/id');
+        $dependencies->request->attributes->method('get')->willReturnCallback(function($property) {
+            if ($property === '_locale') {
+                return 'fr';
+            }
+        });
         $this->assertEquals('es', $resolver->resolve());
     }
 
 }
 
-class LocalePathResolverTestDependencies
+class LocaleResolverTestDependencies
 {
     /** @var RequestStack|MockObject */
     public $requestStack;
