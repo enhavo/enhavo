5a82ccb8eaba3914cdec468e7f495db7444828c8
Gerhard Seidel <gseidel.message@googlemail.com>
2020-09-11 19:52:55 +0200

    Fix purifier writeable path

--- src/Enhavo/Bundle/FormBundle/Tests/Formatter/HtmlSanitizerTest.php
+++ src/Enhavo/Bundle/FormBundle/Tests/Formatter/HtmlSanitizerTest.php
@@ -8,6 +8,7 @@
 
 namespace Enhavo\Bundle\FormBundle\Tests\Formatter;
 
+use Enhavo\Bundle\AppBundle\Filesystem\Filesystem;
 use Enhavo\Bundle\FormBundle\Formatter\HtmlSanitizer;
 use PHPUnit\Framework\TestCase;
 
@@ -15,7 +16,7 @@ class HtmlSanitizerTest extends TestCase
 {
     public function testSanitize()
     {
-        $sanitizer = new HtmlSanitizer();
+        $sanitizer = new HtmlSanitizer(sys_get_temp_dir().'/purifier/serialization', new Filesystem());
 
         $this->assertEquals('<p>Test</p>', $sanitizer->sanitize('<script>Hello</script><p>Test</p>'));
         $this->assertEquals('Hello<p>Test</p>', $sanitizer->sanitize('<iframe>Hello</iframe><p>Test</p>'));
