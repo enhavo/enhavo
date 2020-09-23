038ec8163d2706d9205ba53c8bc3e02da11b2ce7
indivisualvj <account@bennson.de>
2020-09-23 12:08:40 +0200

    make subscription selectable in SubscribeBlock

--- src/Enhavo/Bundle/NewsletterBundle/Entity/SubscribeBlock.php
+++ src/Enhavo/Bundle/NewsletterBundle/Entity/SubscribeBlock.php
@@ -6,5 +6,24 @@ use Enhavo\Bundle\BlockBundle\Entity\AbstractBlock;
 
 class SubscribeBlock extends AbstractBlock
 {
+    /** @var string|null */
+    private $subscription;
+
+    /**
+     * @return string|null
+     */
+    public function getSubscription(): ?string
+    {
+        return $this->subscription;
+    }
+
+    /**
+     * @param string|null $subscription
+     */
+    public function setSubscription(?string $subscription): void
+    {
+        $this->subscription = $subscription;
+    }
+
 
 }
--- src/Enhavo/Bundle/NewsletterBundle/Form/Type/SubscribeBlockType.php
+++ src/Enhavo/Bundle/NewsletterBundle/Form/Type/SubscribeBlockType.php
@@ -2,6 +2,7 @@
 
 namespace Enhavo\Bundle\NewsletterBundle\Form\Type;
 
+use Enhavo\Bundle\NewsletterBundle\Entity\SubscribeBlock;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -10,13 +11,17 @@ class SubscribeBlockType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $builder
+            ->add('subscription', SubscriptionType::class, [
 
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'Enhavo\Bundle\NewsletterBundle\Entity\SubscribeBlock'
+            'data_class' => SubscribeBlock::class,
         ));
     }
 
