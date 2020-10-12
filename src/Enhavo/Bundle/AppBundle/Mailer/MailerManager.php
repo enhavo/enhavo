c70e13f836ac92f3331f670543746bbaf42d8631
Gerhard Seidel <gseidel.message@googlemail.com>
2020-10-12 21:42:01 +0200

    Change MailerManager api and config (#1060)

---
+++ src/Enhavo/Bundle/AppBundle/Mailer/MailerManager.php
@@ -0,0 +1,130 @@
+<?php
+
+
+namespace Enhavo\Bundle\AppBundle\Mailer;
+
+use Enhavo\Bundle\AppBundle\Exception\MailNotFoundException;
+use Enhavo\Bundle\MediaBundle\Model\FileInterface;
+use Swift_Mailer;
+use Swift_Message;
+use Swift_Attachment;
+use Symfony\Component\HttpFoundation\File\File;
+use Twig\Environment;
+
+class MailerManager
+{
+    /** @var Swift_Mailer */
+    private $mailer;
+
+    /** @var Environment */
+    private $environment;
+
+    /** @var array */
+    private $defaultConfig;
+
+    /** @var array */
+    private $mailsConfig;
+
+    /** @var string */
+    private $model;
+
+    /**
+     * MailerManager constructor.
+     * @param Swift_Mailer $mailer
+     * @param Environment $environment
+     * @param array $defaultConfig
+     * @param array $mailsConfig
+     * @param $model
+     */
+    public function __construct(Swift_Mailer $mailer, Environment $environment, array $defaultConfig, array $mailsConfig, $model)
+    {
+        $this->mailer = $mailer;
+        $this->environment = $environment;
+        $this->defaultConfig = $defaultConfig;
+        $this->mailsConfig = $mailsConfig;
+        $this->model = $model;
+    }
+
+    public function sendMail(string $key, $resource, array $attachments = []): int
+    {
+        $message = $this->createMail($key, $resource, $attachments);
+        return $this->sendMessage($message);
+    }
+
+    public function createMail(string $key, $resource, array $attachments = []): Message
+    {
+        if (!isset($this->mailsConfig[$key])) {
+            throw new MailNotFoundException(sprintf('Mail with key "%s" does not exist in enhavo_app.mailer.mails', $key));
+        }
+
+        $message = $this->createMessage();
+
+        $message->setFrom($this->mailsConfig[$key]['from'] ?? $this->defaultConfig['from']);
+        $message->setTo($this->mailsConfig[$key]['to'] ?? $this->defaultConfig['to']);
+        $message->setSenderName($this->mailsConfig[$key]['name'] ?? $this->defaultConfig['name']);
+
+        $message->setSubject($this->mailsConfig[$key]['subject']);
+        $message->setTemplate($this->mailsConfig[$key]['template']);
+        $message->setContext([
+            'resource' => $resource,
+        ]);
+        $message->setAttachments($attachments);
+        $message->setContentType($this->mailsConfig[$key]['content_type']);
+
+        return $message;
+    }
+
+    public function createMessage(): Message
+    {
+        return new $this->model;
+    }
+
+    public function sendMessage(Message $message): int
+    {
+        $swiftMessage = $this->convert($message);
+        return $this->send($swiftMessage);
+    }
+
+    public function convert(Message $message): Swift_Message
+    {
+        $swiftMessage = new Swift_Message();
+        $swiftMessage->setSubject($this->renderString($message->getSubject(), $message->getContext()))
+            ->setFrom(
+                $this->renderString($message->getFrom(), $message->getContext()),
+                $this->renderString($message->getSenderName(), $message->getContext())
+            )
+            ->setTo($this->renderString($message->getTo(), $message->getContext()))
+        ;
+
+        $template = $this->environment->load($message->getTemplate());
+
+        if ($message->getContentType() === Message::CONTENT_TYPE_MIXED) {
+            $swiftMessage->setBody($template->renderBlock('text_plain', $message->getContext()), Message::CONTENT_TYPE_PLAIN);
+            $swiftMessage->addPart($template->renderBlock('text_html', $message->getContext()), Message::CONTENT_TYPE_HTML);
+        } else {
+            $swiftMessage->setBody($template->render($message->getContext()), $message->getContentType());
+        }
+
+        foreach ($message->getAttachments() as $attachment) {
+            if ($attachment instanceof FileInterface) {
+                $swiftMessage->attach(Swift_Attachment::fromPath($attachment->getFilename()));
+            } else if ($attachment instanceof File) {
+                $swiftMessage->attach(Swift_Attachment::fromPath($attachment->getRealPath()));
+            } else {
+                $swiftMessage->attach(Swift_Attachment::fromPath($attachment));
+            }
+        }
+
+        return $swiftMessage;
+    }
+
+    public function send(Swift_Message $message): int
+    {
+        return $this->mailer->send($message);
+    }
+
+    private function renderString(string $string, array $context)
+    {
+        return $this->environment->createTemplate($string)->render($context);
+    }
+}
---
+++ src/Enhavo/Bundle/AppBundle/Mailer/Message.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace Enhavo\Bundle\AppBundle\Mailer;
+
+class Message
+{
+    const CONTENT_TYPE_MIXED = 'multipart/mixed';
+    const CONTENT_TYPE_HTML = 'text/html';
+    const CONTENT_TYPE_PLAIN = 'text/plain';
+
+    /** @var string */
+    private $from;
+
+    /** @var string */
+    private $senderName;
+
+    /** @var string */
+    private $to;
+
+    /** @var string */
+    private $subject;
+
+    /** @var string */
+    private $template;
+
+    /** @var array */
+    private $context;
+
+    /** @var array */
+    private $attachments;
+
+    /** @var string */
+    private $contentType;
+
+    public function __construct(
+        string $from = null,
+        string $senderName = null,
+        string $to = null,
+        string $subject = null,
+        string $template = null,
+        array $context = [],
+        array $attachments = [],
+        string $contentType = self::CONTENT_TYPE_PLAIN
+    ) {
+        $this->from = $from;
+        $this->senderName = $senderName;
+        $this->to = $to;
+        $this->subject = $subject;
+        $this->template = $template;
+        $this->context = $context;
+        $this->attachments = $attachments;
+        $this->contentType = $contentType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFrom(): ?string
+    {
+        return $this->from;
+    }
+
+    /**
+     * @param string $from
+     */
+    public function setFrom(?string $from): void
+    {
+        $this->from = $from;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSenderName(): ?string
+    {
+        return $this->senderName;
+    }
+
+    /**
+     * @param string $senderName
+     */
+    public function setSenderName(?string $senderName): void
+    {
+        $this->senderName = $senderName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTo(): ?string
+    {
+        return $this->to;
+    }
+
+    /**
+     * @param string $to
+     */
+    public function setTo(?string $to): void
+    {
+        $this->to = $to;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     */
+    public function setSubject(?string $subject): void
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplate(): ?string
+    {
+        return $this->template;
+    }
+
+    /**
+     * @param string $template
+     */
+    public function setTemplate(?string $template): void
+    {
+        $this->template = $template;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttachments(): array
+    {
+        return $this->attachments;
+    }
+
+    /**
+     * @param array $attachments
+     */
+    public function setAttachments(array $attachments): void
+    {
+        $this->attachments = $attachments;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContentType(): string
+    {
+        return $this->contentType;
+    }
+
+    /**
+     * @param string $contentType
+     */
+    public function setContentType(string $contentType): void
+    {
+        $this->contentType = $contentType;
+    }
+
+    /**
+     * @return array
+     */
+    public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    /**
+     * @param array $context
+     */
+    public function setContext(array $context): void
+    {
+        $this->context = $context;
+    }
+
+    /**
+     * @param mixed $context
+     */
+    public function addContext($context)
+    {
+        $this->context[] = $context;
+    }
+
+    /**
+     * @param mixed $context
+     */
+    public function removeContext($context)
+    {
+        if (false !== $key = array_search($context, $this->context, true)) {
+            array_splice($this->context, $key, 1);
+        }
+    }
+
+    /**
+     * @param mixed $attachment
+     */
+    public function addAttachment($attachment)
+    {
+        $this->attachments[] = $attachment;
+    }
+
+    /**
+     * @param mixed $attachment
+     */
+    public function removeAttachment($attachment)
+    {
+        if (false !== $key = array_search($attachment, $this->attachments, true)) {
+            array_splice($this->attachments, $key, 1);
+        }
+    }
+}
