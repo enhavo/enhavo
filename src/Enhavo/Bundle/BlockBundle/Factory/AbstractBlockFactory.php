9ea0cd84fb5ee7e9564583dda560d806a946a15d
indivisual <ennyennson@gmail.com>
2020-10-13 10:35:06 +0200

    fix block factory after type update (#1061)

--- src/Enhavo/Bundle/BlockBundle/Factory/AbstractBlockFactory.php
+++ src/Enhavo/Bundle/BlockBundle/Factory/AbstractBlockFactory.php
@@ -9,6 +9,7 @@
 namespace Enhavo\Bundle\BlockBundle\Factory;
 
 use Enhavo\Bundle\BlockBundle\Model\BlockInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
