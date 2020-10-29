074e462345761317a2ed95b2cd946be95c145075
indivisualvj <account@bennson.de>
2020-10-29 10:07:40 +0100

    cleanup

--- src/Enhavo/Bundle/UserBundle/Security/Authentication/AuthenticationSuccessTrait.php
+++ src/Enhavo/Bundle/UserBundle/Security/Authentication/AuthenticationSuccessTrait.php
@@ -11,8 +11,6 @@
 
 namespace Enhavo\Bundle\UserBundle\Security\Authentication;
 
-
-use Enhavo\Bundle\UserBundle\Event\UserEvents;
 use Enhavo\Bundle\UserBundle\Event\UserLoginEvent;
 use Enhavo\Bundle\UserBundle\Model\UserInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -27,6 +25,6 @@ trait AuthenticationSuccessTrait
     {
         /** @var UserInterface $user */
         $user = $token->getUser();
-        $this->eventDispatcher->dispatch(UserEvents::LOGIN, new UserLoginEvent($user));
+        $this->eventDispatcher->dispatch(new UserLoginEvent($user));
     }
 }
