c52a858d3e888354d9c625ce27744cd5002a3438
Gerhard Seidel <gseidel.message@googlemail.com>
2021-02-04 12:34:09 +0800

    Fix publishing check for article and page (#1149)

--- src/Enhavo/Bundle/PageBundle/Controller/PageController.php
+++ src/Enhavo/Bundle/PageBundle/Controller/PageController.php
@@ -4,13 +4,19 @@ namespace Enhavo\Bundle\PageBundle\Controller;
 
 use Enhavo\Bundle\AppBundle\Controller\ResourceController;
 use Enhavo\Bundle\AppBundle\Template\TemplateTrait;
+use Enhavo\Bundle\PageBundle\Entity\Page;
+use Enhavo\Bundle\PageBundle\Model\PageInterface;
 
 class PageController extends ResourceController
 {
     use TemplateTrait;
 
-    public function showResourceAction($contentDocument)
+    public function showResourceAction(Page $contentDocument)
     {
+        if (!$contentDocument->isPublished()) {
+            throw $this->createNotFoundException();
+        }
+
         return $this->render($this->getTemplate('theme/resource/page/show.html.twig'), array(
             'data' => $contentDocument
         ));
