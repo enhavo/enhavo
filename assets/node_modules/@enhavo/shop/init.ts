fc004fcd886a0cdfd630d4c34fd15943af3a328b
Gerhard Seidel <gseidel.message@googlemail.com>
2021-04-01 11:57:22 +0800

    Fix path

--- assets/node_modules/@enhavo/shop/init.ts
+++ assets/node_modules/@enhavo/shop/init.ts
@@ -2,7 +2,7 @@ import * as $ from "jquery";
 import '@enhavo/shop/assets/styles/block.scss';
 import '@enhavo/shop/assets/styles/shop.scss';
 import '@enhavo/shop/assets/styles/user.scss';
-import Cart from "@enhavo/shop/Cart/Cart";
+import Cart from "@enhavo/shop/cart/Cart";
 
 $(() => {
     (new Cart).init(document.body);
