fdb6c77c84181686e171ec5e7c36d346e099bf4d
Gerhard Seidel <gseidel.message@googlemail.com>
2021-04-02 11:09:48 +0800

    Add setting function to twig (#1189)

--- src/Enhavo/Bundle/SettingBundle/Exception/SettingNotExists.php
+++ src/Enhavo/Bundle/SettingBundle/Exception/SettingNotExists.php
@@ -4,5 +4,11 @@ namespace Enhavo\Bundle\SettingBundle\Exception;
 
 class SettingNotExists extends \Exception
 {
+    public static function keyNotFound($key) {
+        return new self(sprintf('Setting with key "%s" is not configured', $key));
+    }
 
+    public static function entityNotFound() {
+        return new self('Setting config exists, but no entity found. Maybe you forgot to execute bin/console enhavo:init');
+    }
 }
