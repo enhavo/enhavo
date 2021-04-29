737a4f1f86cec90c4a385221a845cc564c7e6226
Gerhard Seidel <gseidel.message@googlemail.com>
2021-04-29 14:37:51 +0800

    Implement VueForm submitData

--- src/Enhavo/Bundle/VueFormBundle/Tests/Form/VueFormTest.php
+++ src/Enhavo/Bundle/VueFormBundle/Tests/Form/VueFormTest.php
@@ -3,10 +3,24 @@
 namespace Enhavo\Bundle\VueFormBundle\Tests\Form;
 
 use Enhavo\Bundle\VueFormBundle\Form\VueForm;
-use PHPUnit\Framework\TestCase;
+use Enhavo\Bundle\VueFormBundle\Form\VueType\FormVueType;
+use Enhavo\Bundle\VueFormBundle\Form\VueType\TextVueType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Test\TypeTestCase;
 
-class VueFormTest extends TestCase
+class VueFormTest extends TypeTestCase
 {
+    use VueTypeTestTrait;
+
+    protected function getTypeExtensions()
+    {
+        return [
+            $this->createVueTypeExtension([new FormVueType(), new TextVueType()])
+        ];
+    }
+
     public function createInstance(VueFormDependencies $dependencies)
     {
         $instance = new VueForm();
@@ -24,11 +38,71 @@ class VueFormTest extends TestCase
         $dependencies = $this->createDependencies();
         $vueForm = $this->createInstance($dependencies);
 
-        $this->assertTrue(true);
+        $form = $this->factory->create(TestType::class);
+
+        $data = $vueForm->createData($form->createView());
+
+        $this->assertCount(2, $data['children']);
+        $this->assertTrue($data['root']);
+    }
+
+    public function testSubmitData()
+    {
+        $dependencies = $this->createDependencies();
+        $vueForm = $this->createInstance($dependencies);
+
+        $data = [
+            'name' => 'my_form_name',
+            'compound' => true,
+            'children' => [
+                'child_form' => [
+                    'name' => 'child_form',
+                    'value' => 'test',
+                    'compound' => false,
+                ],
+                'other_child_form' => [
+                    'name' => 'other_child_form',
+                    'value' => '',
+                    'compound' => true,
+                    'children' => [
+                        'deep_child' => [
+                            'name' => 'deep_child',
+                            'value' => 'foobar',
+                            'compound' => false,
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $submittedData = $vueForm->submit($data);
+
+        $this->assertIsArray($submittedData);
+        $this->assertEquals('test', $submittedData['child_form']);
+        $this->assertEquals('foobar', $submittedData['other_child_form']['deep_child']);
     }
 }
 
 class VueFormDependencies
 {
 
+}
+
+class TestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('property_one', TextType::class);
+        $builder->add('property_two', SubTestType::class);
+
+    }
+}
+
+class SubTestType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('sub_property_one', TextType::class);
+        $builder->add('sub_property_two', TextType::class);
+    }
 }
