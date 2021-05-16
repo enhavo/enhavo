af894cf1ab425802ffdba6c9077014844ed2ffb3
Gerhard Seidel <gseidel.message@googlemail.com>
2021-05-16 17:17:48 +0800

    Read webpack config in every theme folder (#1261)
    * Read webpack config in every theme folder
    
    * Remove webpack from config
    
    * Remove webpack from config
    
    (cherry picked from commit bb2170de2061e08982593aa9dab3c5389cbd4125)

---
+++ assets/theme/base/webpack.config.js
@@ -0,0 +1,18 @@
+const EnhavoEncore = require('@enhavo/core/EnhavoEncore');
+const AppThemePackage = require('@enhavo/app/Encore/AppThemePackage');
+const ThemePackage = require('@enhavo/theme/encore/ThemePackage');
+const ThemeLoader = require('@enhavo/theme/encore/ThemeLoader');
+const path = require('path');
+
+EnhavoEncore.add(
+    'base',
+    [ new AppThemePackage({  themePath: __dirname }), new ThemePackage(ThemeLoader)],
+    Encore => {
+        Encore
+            .addEntry('base', './base')
+
+    },
+    config => {}
+);
+
+module.exports = EnhavoEncore.export();
