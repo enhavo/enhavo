04e1d5990adc1757f0c72d122cd699de1cc6ba75
Gerhard Seidel <gseidel.message@googlemail.com>
2021-11-28 17:36:42 +0100

    Add video model to handle youtube and vimeo details

---
+++ src/Enhavo/Bundle/ContentBundle/Factory/VideoFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Enhavo\Bundle\ContentBundle\Factory;
+
+use Enhavo\Bundle\ContentBundle\Exception\VideoException;
+use Enhavo\Bundle\ContentBundle\Model\Video;
+use Enhavo\Bundle\ContentBundle\Video\ProviderInterface;
+
+class VideoFactory
+{
+    /** @var ProviderInterface[] */
+    private $providers;
+
+    /**
+     * @param ProviderInterface $provider
+     */
+    public function addProvider(ProviderInterface $provider)
+    {
+        $this->providers[] = $provider;
+    }
+
+    public function create($url): Video
+    {
+        foreach ($this->providers as $provider) {
+            if ($provider->isSupported($url)) {
+                return $provider->create($url);
+            }
+        }
+
+        throw VideoException::noProviderFound($url);
+    }
+
+    public function isSupported($url): bool
+    {
+        foreach ($this->providers as $provider) {
+            if ($provider->isSupported($url)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
