7a30fc73d661798339d59669075139dc3e38b62e
Gerhard Seidel <gseidel.message@googlemail.com>
2021-11-30 13:34:35 +0100

    Fix search result and add elastic version (#1448)
    (cherry picked from commit 86f39146c48683364b83c0b910284332d5dd4705)

--- src/Enhavo/Bundle/SearchBundle/Controller/SearchController.php
+++ src/Enhavo/Bundle/SearchBundle/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace Enhavo\Bundle\SearchBundle\Controller;
 
+use Enhavo\Bundle\AppBundle\Template\TemplateManager;
 use Enhavo\Bundle\SearchBundle\Engine\EngineInterface;
 use Enhavo\Bundle\SearchBundle\Engine\Filter\Filter;
 use Enhavo\Bundle\SearchBundle\Result\ResultConverter;
@@ -11,24 +12,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends AbstractController
 {
-    /**
-     * @var ResultConverter
-     */
+    /** @var ResultConverter */
     private $resultConverter;
 
-    /**
-     * @var EngineInterface
-     */
+    /** @var EngineInterface */
     private $searchEngine;
 
+    /** @var TemplateManager */
+    private $templateManager;
+
     /**
-     * SearchController constructor.
      * @param ResultConverter $resultConverter
+     * @param EngineInterface $searchEngine
+     * @param TemplateManager $templateManager
      */
-    public function __construct(ResultConverter $resultConverter, EngineInterface $searchEngine)
+    public function __construct(ResultConverter $resultConverter, EngineInterface $searchEngine, TemplateManager $templateManager)
     {
         $this->resultConverter = $resultConverter;
         $this->searchEngine = $searchEngine;
+        $this->templateManager = $templateManager;
     }
 
     /**
@@ -39,19 +41,50 @@ class SearchController extends AbstractController
      */
     public function submitAction(Request $request)
     {
-        $template = 'EnhavoSearchBundle:Admin:Search/result.html.twig';
+        $configuration = $this->createConfiguration($request);
 
         $term = $request->get('q', '');
+        $page = $request->get('page', 1);
 
         $filter = new Filter();
         $filter->setTerm($term);
+        $filter->setLimit(100);
 
-        $results = $this->searchEngine->searchPaginated($filter);
-        $results = $this->resultConverter->convert($results, $term);
+        $pagination = $this->searchEngine->searchPaginated($filter);
 
-        return $this->render($template, [
+        $pagination->setCurrentPage($page);
+        $pagination->setMaxPerPage($configuration->getMaxPerPage());
+
+        $results = $this->resultConverter->convert($pagination, $term);
+
+        return $this->render($this->templateManager->getTemplate($configuration->getTemplate()), [
             'results' => $results,
+            'pagination' => $pagination,
             'term' => $term
         ]);
+    }
+
+    private function createConfiguration(Request $request): SearchConfiguration
+    {
+        $configuration = new SearchConfiguration();
+
+        $config =  $request->attributes->get('_config');
+
+        if ($config === null) {
+            throw new \InvalidArgumentException('The config attribute is mandatory');
+        } elseif (!is_array($config)) {
+            throw new \InvalidArgumentException('The config has to be an array');
+        }
+
+        if (!isset($config['template'])) {
+            throw new \InvalidArgumentException('Template must be set');
+        }
+        $configuration->setTemplate($config['template']);
+
+        if (isset($config['max_per_page'])) {
+            $configuration->setMaxPerPage($config['max_per_page']);
+        }
+
+        return $configuration;
     }
 }
