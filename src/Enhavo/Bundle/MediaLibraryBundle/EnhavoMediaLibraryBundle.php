02dc5b3f216f4c612c0a825b6169fbcb4369800a
visual.indi <account@bennson.de>
2022-02-22 09:10:48 +0100

    media library initial

---
+++ src/Enhavo/Bundle/MediaLibraryBundle/EnhavoMediaLibraryBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Enhavo\Bundle\MediaLibraryBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EnhavoMediaLibraryBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
