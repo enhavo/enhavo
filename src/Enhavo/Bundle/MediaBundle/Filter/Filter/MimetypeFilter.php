de5efdcc41808852ac415c1e9c72d9a4fb592f72
visual.indi <account@bennson.de>
2022-03-17 15:13:51 +0100

    previews

---
+++ src/Enhavo/Bundle/MediaBundle/Filter/Filter/MimetypeFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Enhavo\Bundle\MediaBundle\Filter\Filter;
+
+use Enhavo\Bundle\MediaBundle\Filter\AbstractFilter;
+use Enhavo\Bundle\MediaBundle\Media\FormatManager;
+use Enhavo\Bundle\MediaBundle\Model\FilterSetting;
+
+class MimetypeFilter extends AbstractFilter
+{
+    /**
+     * @var FormatManager
+     */
+    private $formatManager;
+
+    /**
+     * ParentFilter constructor.
+     * @param FormatManager $formatManager
+     */
+    public function __construct(FormatManager $formatManager)
+    {
+        $this->formatManager = $formatManager;
+    }
+
+    public function apply($file, FilterSetting $setting)
+    {
+        $content = $this->getContent($file);
+        $mimeType = $file->getMimeType();
+        $format = $setting->getSetting('format');
+        $mimeTypes = $setting->getSetting('mimetypes', []);
+        $settings = $this->formatManager->getFormatSettings($format);
+        foreach ($settings as $setting) {
+            if (in_array($mimeType, $mimeTypes)) {
+                $format = $this->formatManager->getFilter($setting->getType());
+                $format->apply($content, $setting);
+            }
+        }
+    }
+
+    public function getType()
+    {
+        return 'mimetype';
+    }
+}
