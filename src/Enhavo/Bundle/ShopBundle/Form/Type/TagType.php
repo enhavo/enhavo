538adc5ad70283a839cc6544edf487f7becec0b2
Fabian Liebl <fabian.liebl@xq-web.de>
2022-03-29 10:36:40 +0200

    [ShopBundle] Fix for copy/paste errors in taxonomy routes

--- src/Enhavo/Bundle/ShopBundle/Form/Type/TagType.php
+++ src/Enhavo/Bundle/ShopBundle/Form/Type/TagType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Enhavo\Bundle\ArticleBundle\Form\Type;
+namespace Enhavo\Bundle\ShopBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
