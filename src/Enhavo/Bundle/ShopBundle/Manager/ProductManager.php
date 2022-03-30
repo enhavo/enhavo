711319d706dd0a233d526ffddaa6766ea178a72d
Requiem4aPr0 <philipp.sester@xq-web.de>
2022-03-30 16:34:45 +0200

    check generated code is unique in database

--- src/Enhavo/Bundle/ShopBundle/Manager/ProductManager.php
+++ src/Enhavo/Bundle/ShopBundle/Manager/ProductManager.php
@@ -8,13 +8,35 @@
 
 namespace Enhavo\Bundle\ShopBundle\Manager;
 
-use Enhavo\Bundle\AppBundle\Util\NameTransformer;
+use Doctrine\ORM\EntityManagerInterface;
 use Enhavo\Bundle\RoutingBundle\Slugifier\Slugifier;
+use Enhavo\Bundle\ShopBundle\Entity\Product;
 
 class ProductManager
 {
+    private EntityManagerInterface $em;
+
+    /**
+     * ProductManager constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function generateCode($name)
     {
-        return substr(md5(microtime()),rand(0,26),4) . '-' . Slugifier::slugify($name);
+        do {
+             $code = substr(md5(microtime()),rand(0,26),4) . '-' . Slugifier::slugify($name);
+        } while (!$this->isUnique($code));
+        return $code;
+    }
+
+    private function isUnique($code) {
+        $products = $this->em->getRepository(Product::class)->findBy([
+            'code' => $code
+        ]);
+        return empty($products);
     }
 }
--- src/Enhavo/Bundle/ShopBundle/Manager/ProductVariantManager.php
+++ src/Enhavo/Bundle/ShopBundle/Manager/ProductVariantManager.php
@@ -8,11 +8,25 @@
 
 namespace Enhavo\Bundle\ShopBundle\Manager;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Enhavo\Bundle\RoutingBundle\Slugifier\Slugifier;
+use Enhavo\Bundle\ShopBundle\Entity\Product;
 use Enhavo\Bundle\ShopBundle\Entity\ProductOption;
+use Enhavo\Bundle\ShopBundle\Entity\ProductVariant;
 
 class ProductVariantManager
 {
+    private EntityManagerInterface $em;
+
+    /**
+     * ProductManager constructor.
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function generateCode($name, $options): string
     {
         $optionString = [];
@@ -21,6 +35,17 @@ class ProductVariantManager
             $optionString[] = $option->getCode();
         }
 
-        return substr(md5(microtime()),rand(0,26),4) . '-' . Slugifier::slugify($name) . '-' . implode('-', $optionString);
+        do {
+            $code = substr(md5(microtime()),rand(0,26),4) . '-' . Slugifier::slugify($name) . '-' . implode('-', $optionString);
+        } while (!$this->isUnique($code));
+        return $code;
+    }
+
+    private function isUnique($code): bool
+    {
+        $variants = $this->em->getRepository(ProductVariant::class)->findBy([
+            'code' => $code
+        ]);
+        return empty($variants);
     }
 }
