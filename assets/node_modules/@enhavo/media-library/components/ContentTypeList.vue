2e8e6b45bca78d11ca578abda7f4184f89d0a577
visual.indi <account@bennson.de>
2022-03-31 14:45:08 +0200

    vue fixes

---
+++ assets/node_modules/@enhavo/media-library/components/ContentTypeList.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <content-type
+            v-for="contentType of contentTypes"
+            :contentType="contentType"
+        ></content-type>
+    </ul>
+</template>
+
+<script lang="ts">
+import {Options, Prop, Vue} from "vue-property-decorator";
+import {ContentType} from "../Data";
+
+@Options({})
+export default class extends Vue {
+    @Prop()
+    contentTypes: Array<ContentType>;
+}
+</script>
---
+++ assets/node_modules/@enhavo/media-library/components/TagList.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul>
+        <tag
+            v-for="tag of tags"
+            :tag="tag"
+        ></tag>
+    </ul>
+</template>
+
+<script lang="ts">
+import {Options, Prop, Vue} from "vue-property-decorator";
+import {Tag} from "../Data";
+
+@Options({})
+export default class extends Vue {
+    @Prop()
+    tags: Array<Tag>;
+}
+</script>
