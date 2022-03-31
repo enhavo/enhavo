e54ab07a308e101d65ed6cc4bf91fe1fb6dadd48
visual.indi <account@bennson.de>
2022-03-31 14:52:01 +0200

    remove media library from media bundle

--- assets/node_modules/@enhavo/media/entrypoint/media-library.ts
+++
@@ -1,6 +0,0 @@
-import Container from "@enhavo/dependency-injection"
-(async () => {
-    await Container.init();
-    (await Container.get('@enhavo/media/media-library/MediaLibraryApp')).init();
-    (await Container.get('@enhavo/app/vue/VueApp')).init('app', await Container.get('@enhavo/media/media-library/components/ApplicationComponent.vue'));
-})();
