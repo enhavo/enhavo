60ecc4ae6eec9e039eaae5fb5b1913585cf7f94c
visual.indi <account@bennson.de>
2022-04-11 15:32:39 +0200

    ui fixes (#2)
    * icons in the back when preview is loaded on thumbnails
    
    * list view type active state and styles
    
    * fixed sorting column state
    
    * fix view getter
    
    Co-authored-by: Matthias Weß <mw@bnize.com>

--- assets/node_modules/@enhavo/media-library/components/ThumbnailFile.vue
+++ assets/node_modules/@enhavo/media-library/components/ThumbnailFile.vue
@@ -1,8 +1,10 @@
 <template>
     <li :class="{'active': file.selected}" @click="onClick()">
         <span v-if="file.selected"><i class="icon icon-check"></i></span>
-        <div :style="{ backgroundImage: 'url(' + file.previewImageUrl + ')' }" class="img">
-            <span><i class="icon" :class="file.icon"></i></span>
+        <div class="img">
+            <div class="background" :style="{ backgroundImage: 'url(' + file.previewImageUrl + ')' }">
+            </div>
+            <span ><i class="icon" :class="file.icon"></i></span>
         </div>
         <div class="title">{{ file.label }}</div>
     </li>
