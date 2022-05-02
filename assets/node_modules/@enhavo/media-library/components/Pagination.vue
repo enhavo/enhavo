2d9b2086b8d76dee6a8529f447b8aa323a1bdad8
visual.indi <account@bennson.de>
2022-05-02 10:49:07 +0200

    show prev/next on pagination and also show maximum number of three pages

--- assets/node_modules/@enhavo/media-library/components/Pagination.vue
+++ assets/node_modules/@enhavo/media-library/components/Pagination.vue
@@ -1,25 +1,23 @@
 <template>
     <div class="media-library-pagination">
         <ul>
-            <li v-for="page in mediaLibrary.data.pages"
+            <li v-if="hasPrevious()" @click="setPage(this.previousPage())">&lt;</li>
+            <li v-else>&nbsp;</li>
+            <li v-for="page in this.getPages()"
                 @click="setPage(page)"
                 :class="isActivePage(page)?'active':''">
                 {{ page }}
             </li>
+            <li v-if="hasNext()" @click="setPage(this.nextPage())">&gt;</li>
+            <li v-else>&nbsp;</li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-import * as $ from 'jquery';
 import {Inject, Options, Vue, Watch} from "vue-property-decorator";
 import '@enhavo/app/assets/styles/view.scss';
-import Translator from "@enhavo/core/Translator";
-import Router from "@enhavo/core/Router";
-import {Column, File} from "@enhavo/media-library/Data";
 import MediaLibrary from "@enhavo/media-library/MediaLibrary";
-import ListView from "@enhavo/media-library/components/ListView.vue";
-import ThumbnailView from "@enhavo/media-library/components/ThumbnailView.vue";
 
 @Options({})
 export default class extends Vue {
@@ -33,6 +31,38 @@ export default class extends Vue {
 
     isActivePage(page: number) {
         return this.mediaLibrary.getActivePage() == page;
+    }
+
+    getPages() {
+        let current = this.mediaLibrary.getActivePage();
+        let pages = this.mediaLibrary.getPages().length;
+        if (pages > 3) {
+            if (current < 2) {
+                return [1, 2, 3];
+            } else if (current > pages - 2) {
+                return [pages - 2, pages - 1, pages];
+            }
+
+            return [current - 1, current, current + 1];
+        }
+
+        return this.mediaLibrary.getPages();
+    }
+
+    previousPage() {
+        return this.mediaLibrary.getActivePage() - 1;
+    }
+
+    nextPage() {
+        return (this.mediaLibrary.getActivePage() + 1);
+    }
+
+    hasPrevious() {
+        return this.mediaLibrary.getActivePage() > 1;
+    }
+
+    hasNext() {
+        return this.mediaLibrary.getActivePage() < this.mediaLibrary.getPages().length;
     }
 }
 </script>
