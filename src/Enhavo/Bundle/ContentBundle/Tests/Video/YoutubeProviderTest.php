121c4070df9de197f21375ad048ebca7dcd683d3
Gerhard Seidel <gseidel.message@googlemail.com>
2022-07-08 15:03:37 +0200

    Check multiple urls for youtube videos (#1608)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | yes
    | New feature?  | no
    | Backport      | 0.10
    | License       | MIT
    
    Check multiple urls for youtube thumbnails, because not all resolutions exists
    
    (cherry picked from commit 7a83e2be47ba63908591300b2a064f98fa3f9037)
    
    # Conflicts:
    #	src/Enhavo/Bundle/ContentBundle/Tests/Video/YoutubeProviderTest.php
    #	src/Enhavo/Bundle/ContentBundle/Video/YoutubeProvider.php

--- src/Enhavo/Bundle/ContentBundle/Tests/Video/YoutubeProviderTest.php
+++ src/Enhavo/Bundle/ContentBundle/Tests/Video/YoutubeProviderTest.php
@@ -17,13 +17,31 @@ class YoutubeProviderTest extends TestCase
 
     public function testCreateWithoutApiKey()
     {
-        $provider = new YoutubeProvider();
+        $provider = new YoutubeProvider(null, YoutubeProvider::IMAGE_TYPE_MAX, false);
 
         $video = $provider->create('https://www.youtube.com/watch?v=SomeKey');
 
+<<<<<<< HEAD
         $this->assertEquals('http://www.youtube.com/embed/SomeKey', $video->getEmbedUrl());
         $this->assertEquals('http://www.youtube.com/watch?v=SomeKey', $video->getVideoUrl());
         $this->assertEquals('https://i.ytimg.com/vi/SomeKey/maxresdefault.jpg', $video->getThumbnail());
+=======
+        $this->assertEquals('https://www.youtube.com/embed/S0meK3y', $video->getEmbedUrl());
+        $this->assertEquals('https://www.youtube.com/watch?v=S0meK3y', $video->getVideoUrl());
+        $this->assertEquals('https://i.ytimg.com/vi/S0meK3y/maxresdefault.jpg', $video->getThumbnail());
+        $this->assertEquals(YoutubeProvider::PROVIDER_NAME, $video->getProvider());
+    }
+
+    public function testCreateFromShortUrlWithoutApiKey()
+    {
+        $provider = new YoutubeProvider(null, YoutubeProvider::IMAGE_TYPE_MAX, false);
+
+        $video = $provider->create('https://youtu.be/S0meK3y');
+
+        $this->assertEquals('https://www.youtube.com/embed/S0meK3y', $video->getEmbedUrl());
+        $this->assertEquals('https://www.youtube.com/watch?v=S0meK3y', $video->getVideoUrl());
+        $this->assertEquals('https://i.ytimg.com/vi/S0meK3y/maxresdefault.jpg', $video->getThumbnail());
+>>>>>>> 7a83e2be4 (Check multiple urls for youtube videos (#1608))
         $this->assertEquals(YoutubeProvider::PROVIDER_NAME, $video->getProvider());
     }
 }
--- src/Enhavo/Bundle/ContentBundle/Video/YoutubeProvider.php
+++ src/Enhavo/Bundle/ContentBundle/Video/YoutubeProvider.php
@@ -2,13 +2,16 @@
 
 namespace Enhavo\Bundle\ContentBundle\Video;
 
+use Enhavo\Bundle\ContentBundle\Exception\VideoException;
 use Enhavo\Bundle\ContentBundle\Model\Video;
 
 class YoutubeProvider implements ProviderInterface
 {
     const PROVIDER_NAME = 'youtube';
+    const IMAGE_TYPE_MQ = 'mqdefault';
     const IMAGE_TYPE_HQ = 'hqdefault';
-    const IMAGE_TYPE_MAX_RESOLUTION = 'maxresdefault';
+    const IMAGE_TYPE_SQ = 'sddefault';
+    const IMAGE_TYPE_MAX = 'maxresdefault';
 
     /** @var ?string */
     private $apiKey;
@@ -16,13 +19,24 @@ class YoutubeProvider implements ProviderInterface
     /** @var string */
     private $imageType;
 
+<<<<<<< HEAD
     /**
      * @param string $apiKey
      */
     public function __construct(?string $apiKey = null, $imageType = self::IMAGE_TYPE_MAX_RESOLUTION)
+=======
+    /** @var bool */
+    private $checkThumbnailUrl;
+
+    /**
+     * @param string $apiKey
+     */
+    public function __construct(?string $apiKey = null, $imageType = self::IMAGE_TYPE_MAX, $checkThumbnailUrl = true)
+>>>>>>> 7a83e2be4 (Check multiple urls for youtube videos (#1608))
     {
         $this->apiKey = $apiKey;
         $this->imageType = $imageType;
+        $this->checkThumbnailUrl = $checkThumbnailUrl;
     }
 
     public function create(string $url): Video
@@ -34,9 +48,15 @@ class YoutubeProvider implements ProviderInterface
                 self::PROVIDER_NAME,
                 '',
                 '',
+<<<<<<< HEAD
                 sprintf('https://i.ytimg.com/vi/%s/%s.jpg', $videoId, $this->imageType),
                 sprintf("http://www.youtube.com/watch?v=%s", $videoId),
                 sprintf("http://www.youtube.com/embed/%s", $videoId)
+=======
+                $this->getThumbnailUrl($videoId, $this->imageType),
+                sprintf("https://www.youtube.com/watch?v=%s", $videoId),
+                sprintf("https://www.youtube.com/embed/%s", $videoId)
+>>>>>>> 7a83e2be4 (Check multiple urls for youtube videos (#1608))
             );
         }
 
@@ -46,9 +66,15 @@ class YoutubeProvider implements ProviderInterface
             self::PROVIDER_NAME,
             $hash->items[0]->snippet->title,
             str_replace(array("", "<br/>", "<br />"), NULL, $hash->items[0]->snippet->description),
+<<<<<<< HEAD
             sprintf('https://i.ytimg.com/vi/%s/%s.jpg', $hash->items[0]->id, $this->imageType),
             sprintf("http://www.youtube.com/watch?v=%s", $hash->items[0]->id),
             sprintf("http://www.youtube.com/embed/%s", $hash->items[0]->id)
+=======
+            $this->getThumbnailUrl($hash->items[0]->id, $this->imageType),
+            sprintf("https://www.youtube.com/watch?v=%s", $hash->items[0]->id),
+            sprintf("https://www.youtube.com/embed/%s", $hash->items[0]->id)
+>>>>>>> 7a83e2be4 (Check multiple urls for youtube videos (#1608))
         );
     }
 
@@ -56,6 +82,37 @@ class YoutubeProvider implements ProviderInterface
     {
         preg_match("/v=([^&#]*)/", parse_url($url, PHP_URL_QUERY), $videoId);
         return $videoId[1];
+    }
+
+    private function getThumbnailUrl($id, $preferredType)
+    {
+        if (!$this->checkThumbnailUrl) {
+            return sprintf('https://i.ytimg.com/vi/%s/%s.jpg', $id, $preferredType);
+        }
+
+        $types = [self::IMAGE_TYPE_MQ, self::IMAGE_TYPE_SQ, self::IMAGE_TYPE_HQ, self::IMAGE_TYPE_MAX];
+        $key = array_search($preferredType, $types);
+        unset($types[$key]);
+
+        $url = sprintf('https://i.ytimg.com/vi/%s/%s.jpg', $id, $preferredType);
+        if ($this->checkUrl($url)) {
+            return $url;
+        }
+
+        while($type = array_pop($types)) {
+            $url = sprintf('https://i.ytimg.com/vi/%s/%s.jpg', $id, $type);
+            if ($this->checkUrl($url)) {
+                return $url;
+            }
+        }
+
+        throw new VideoException(sprintf('Can\' t resolve thmbnail image for youtube video with id: "%s"', $id));
+    }
+
+    private function checkUrl($url): bool
+    {
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3) == "200";
     }
 
     public function isSupported(string $url): bool
