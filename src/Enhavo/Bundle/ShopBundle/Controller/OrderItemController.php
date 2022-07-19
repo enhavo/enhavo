0bc775581ca6ae8b44c982752839aef4fbf4e49c
Gerhard Seidel <gseidel.message@googlemail.com>
2022-07-19 23:00:05 +0200

    Fix flash message

--- src/Enhavo/Bundle/ShopBundle/Controller/OrderItemController.php
+++ src/Enhavo/Bundle/ShopBundle/Controller/OrderItemController.php
@@ -4,8 +4,29 @@ namespace Enhavo\Bundle\ShopBundle\Controller;
 
 use Enhavo\Bundle\AppBundle\Controller\ResourceControllerTrait;
 use Sylius\Bundle\OrderBundle\Controller\OrderItemController as BaseOrderItemController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrderItemController extends BaseOrderItemController
 {
     use ResourceControllerTrait;
+
+    public function addAction(Request $request): Response
+    {
+        $response = parent::addAction($request);
+        $this->clearFlashMessages();
+        return $response;
+    }
+
+    public function removeAction(Request $request): Response
+    {
+        $response = parent::removeAction($request);
+        $this->clearFlashMessages();
+        return $response;
+    }
+
+    private function clearFlashMessages()
+    {
+        $this->get('session')->getFlashBag()->clear();
+    }
 }
