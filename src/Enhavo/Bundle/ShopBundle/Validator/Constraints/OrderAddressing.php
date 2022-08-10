85e9a19b5c413ba2f47ae849efadcf5207a8dd7c
Gerhard Seidel <gseidel.message@googlemail.com>
2022-08-10 16:33:05 +0200

    Guest checkout (#1628)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | no
    | New feature?  | yes
    | License       | MIT
    
    * Allow guest checkout with email

--- src/Enhavo/Bundle/ShopBundle/Validator/Constraints/OrderAddressing.php
+++ src/Enhavo/Bundle/ShopBundle/Validator/Constraints/OrderAddressing.php
@@ -4,16 +4,8 @@ namespace Enhavo\Bundle\ShopBundle\Validator\Constraints;
 
 use Symfony\Component\Validator\Constraint;
 
-/**
- * @Annotation
- */
 class OrderAddressing extends Constraint
 {
-    public function validatedBy()
-    {
-        return 'order_addressing';
-    }
-
     public function getTargets()
     {
         return self::CLASS_CONSTRAINT;
