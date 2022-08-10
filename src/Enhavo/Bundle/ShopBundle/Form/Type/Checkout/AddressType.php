abddc9e0d8cf956a45bce2843e74c2e6dad568f4
Gerhard Seidel <gseidel.message@googlemail.com>
2022-08-10 16:01:40 +0200

    Guest checkout

--- src/Enhavo/Bundle/ShopBundle/Form/Type/Checkout/AddressType.php
+++ src/Enhavo/Bundle/ShopBundle/Form/Type/Checkout/AddressType.php
@@ -3,10 +3,34 @@
 namespace Enhavo\Bundle\ShopBundle\Form\Type\Checkout;
 
 use Enhavo\Bundle\ShopBundle\Form\Type\AddressSubjectType;
+use Enhavo\Bundle\ShopBundle\Model\OrderInterface;
+use Enhavo\Bundle\UserBundle\Model\UserInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Webmozart\Assert\Assert;
 
 class AddressType extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options): void {
+            $form = $event->getForm();
+            $resource = $event->getData();
+
+            Assert::isInstanceOf($resource, OrderInterface::class);
+
+            /** @var UserInterface $user */
+            $user = $resource->getUser();
+
+            if (null === $user) {
+                $form->add('email', TextType::class);
+            }
+        });
+    }
+
     public function getParent()
     {
         return AddressSubjectType::class;
--- src/Enhavo/Bundle/ShopBundle/Validator/Constraints/OrderAddressingValidator.php
+++ src/Enhavo/Bundle/ShopBundle/Validator/Constraints/OrderAddressingValidator.php
@@ -2,55 +2,25 @@
 
 namespace Enhavo\Bundle\ShopBundle\Validator\Constraints;
 
-use Enhavo\Bundle\ShopBundle\Model\OrderInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotNull;
 use Symfony\Component\Validator\ConstraintValidator;
-use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class OrderAddressingValidator extends ConstraintValidator
 {
-    /**
-     * @var ValidatorInterface
-     */
-    protected $validator;
-
-    public function __construct(ValidatorInterface $validator)
-    {
-        $this->validator = $validator;
+    public function __construct(
+        private ValidatorInterface $validator
+    ){
     }
 
     public function validate($value, Constraint $constraint)
     {
-        if(!$value instanceof OrderInterface) {
-            $this->context->buildViolation('Value should implements OrderInterface')->addViolation();
-        }
-        if($value->getUser() === null) {
-            $emailErrors = $this->validator->validate($value->getEmail(), [new NotNull(), new Email()]);
-            foreach($emailErrors as $error) {
+        if ($value->getUser() === null) {
+            $errors = $this->validator->validate($value->getEmail(), [new NotNull(), new Email()]);
+            foreach ($errors as $error) {
                 $this->context->buildViolation($error->getMessage())->atPath('email')->addViolation();
-            }
-        }
-
-        $shippingAddressErrors = $this->validator->validate($value->getShippingAddress(), $constraint->groups);
-        if(count($shippingAddressErrors)) {
-            /** @var ConstraintViolation $error */
-            foreach($shippingAddressErrors as $error) {
-                $propertyPath = sprintf('shippingAddress.%s', $error->getPropertyPath());
-                $this->context->buildViolation($error->getMessage())->atPath($propertyPath)->addViolation();
-            }
-        }
-
-        if($value->isDifferentBillingAddress()) {
-            $billingAddressErrors = $this->validator->validate($value->getBillingAddress(), $constraint->groups);
-            if(count($billingAddressErrors)) {
-                /** @var ConstraintViolation $error */
-                foreach($billingAddressErrors as $error) {
-                    $propertyPath = sprintf('billingAddress.%s', $error->getPropertyPath());
-                    $this->context->buildViolation($error->getMessage())->atPath($propertyPath)->addViolation();
-                }
             }
         }
     }
