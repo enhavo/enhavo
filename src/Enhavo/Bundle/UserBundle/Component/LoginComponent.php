f6923f11d0458d142e5a59e939cd9500d02a6d32
Gerhard Seidel <gseidel.message@googlemail.com>
2022-12-08 14:26:40 +0100

    Fix packages and missing url field (#1675)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | yes
    | New feature?  | no
    | Doc PR?       | no
    | Backport      | 0.11
    | License       | MIT
    
    * Fix package dependecies
    * Fix push `@enhavo/media-library` to npm
    * Fix missing url field in media library
    * Change Version Number and BG Image
    
    (cherry picked from commit 71df6d0e165e53ecd4f7f7619c50ee67041d911d)
    
    # Conflicts:
    #	src/Enhavo/Bundle/AppBundle/composer.json
    #	src/Enhavo/Bundle/ContentBundle/composer.json
    #	src/Enhavo/Bundle/RedirectBundle/composer.json
    #	src/Enhavo/Bundle/TranslationBundle/composer.json
    #	src/Enhavo/Bundle/UserBundle/Component/LoginComponent.php

---
+++ src/Enhavo/Bundle/UserBundle/Component/LoginComponent.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Enhavo\Bundle\UserBundle\Component;
+
+use Enhavo\Bundle\UserBundle\Configuration\ConfigurationProvider;
+use Enhavo\Bundle\UserBundle\Security\Authentication\AuthenticationError;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
+use Symfony\UX\TwigComponent\Attribute\PreMount;
+
+#[AsTwigComponent('user_login', template: 'theme/component/user/login.html.twig')]
+class LoginComponent
+{
+    #[ExposeInTemplate]
+    public ?string $error = null;
+
+    #[ExposeInTemplate]
+    public ?FormView $form = null;
+
+    #[ExposeInTemplate(name: 'failure_path')]
+    public ?string $failurePath = null;
+
+    #[ExposeInTemplate(name: 'target_path')]
+    public ?string $targetPath = null;
+
+    public function __construct(
+        private RequestStack $requestStack,
+        private FormFactoryInterface $formFactory,
+        private ConfigurationProvider $configurationProvider,
+        private AuthenticationError $authenticationError,
+    ) {
+    }
+
+    #[PreMount]
+    public function preMount(array $data): array
+    {
+        $resolver = new OptionsResolver();
+        $resolver->setDefaults([
+            'config' => null,
+            'error' => null,
+            'form' => null,
+            'form_options' => [],
+            'failure_path' => null,
+            'target_path' => null,
+        ]);
+        return $this->createViewData($resolver->resolve($data));
+    }
+
+    private function createViewData(array $options)
+    {
+        $formClass = $options['form'] ?? $this->configurationProvider->getLoginConfiguration($options['config'])->getFormClass();
+        $formOptions = $options['form_options'] ?? $this->configurationProvider->getLoginConfiguration($options['config'])->getFormOptions();
+
+        $request = $this->requestStack->getMainRequest();
+
+        $form = $this->formFactory->create($formClass, null, $formOptions);
+
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $form->handleRequest($request);
+        }
+
+        return [
+            'error' => $options['error'] ?? $this->authenticationError->getError(),
+            'form' => $form->createView(),
+            'failurePath' => $options['failure_path'],
+            'targetPath' => $options['target_path'],
+        ];
+    }
+
+    private function getError()
+    {
+
+    }
+}
