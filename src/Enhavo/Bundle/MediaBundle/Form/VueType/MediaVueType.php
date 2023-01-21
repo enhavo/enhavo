153a0300d522596f22bcbbcb49ccb29f80ee8449
Gerhard Seidel <gseidel.message@googlemail.com>
2023-01-21 23:21:37 +0800

    Handle and prevent error, Upload progress

--- src/Enhavo/Bundle/MediaBundle/Form/VueType/MediaVueType.php
+++ src/Enhavo/Bundle/MediaBundle/Form/VueType/MediaVueType.php
@@ -2,12 +2,19 @@
 
 namespace Enhavo\Bundle\MediaBundle\Form\VueType;
 
+use Enhavo\Bundle\MediaBundle\Media\MediaManager;
 use Enhavo\Bundle\VueFormBundle\Form\AbstractVueType;
 use Enhavo\Bundle\VueFormBundle\Form\VueData;
 use Symfony\Component\Form\FormView;
 
 class MediaVueType extends AbstractVueType
 {
+    public function __construct(
+        private MediaManager $mediaManager,
+    )
+    {
+    }
+
     public static function supports(FormView $formView): bool
     {
         return in_array('enhavo_media', $formView->vars['block_prefixes']);
@@ -16,14 +23,15 @@ class MediaVueType extends AbstractVueType
     public function buildView(FormView $view, VueData $data)
     {
         $data['upload'] = $view->vars['upload'];
+        $data['multiple'] = $view->vars['multiple'];
         $data['uploadLabel'] = 'Upload';
         $data['buttons'] = null;
-        $data['maxUploadSize'] = null;
+        $data['maxUploadSize'] = $this->mediaManager->getMaxUploadSize();
         $data['editable'] = true;
 
         $data['component'] = $data['component'] == 'form-list' ? 'form-media' : $data['component'];
         $data['itemComponent'] = $data['itemComponent'] == 'form-list-item' ? 'form-media-item' : $data['itemComponent'];
-        $data['componentModel'] = $data['componentModel'] == 'FormList' ? 'MediaForm' : $data['componentModel'];
+        $data['componentModel'] = $data['componentModel'] == 'ListForm' ? 'MediaForm' : $data['componentModel'];
     }
 
     public function finishView(FormView $view, VueData $data)
--- src/Enhavo/Bundle/MediaBundle/Media/MediaManager.php
+++ src/Enhavo/Bundle/MediaBundle/Media/MediaManager.php
@@ -143,4 +143,39 @@ class MediaManager
     {
         return $this->associationFinder->findAssociationsTo($file, FileInterface::class, [Format::class]);
     }
+
+    public function getMaxUploadSize()
+    {
+        static $maxSize = -1;
+
+        if ($maxSize < 0) {
+            // Start with postMaxSize.
+            $postMaxSize = $this->parseSize(ini_get('post_max_size'));
+            if ($postMaxSize > 0) {
+                $maxSize = $postMaxSize;
+            }
+
+            // If upload_max_size is less, then reduce. Except if upload_max_size is
+            // zero, which indicates no limit.
+            $uploadMax = $this->parseSize(ini_get('upload_max_filesize'));
+            if ($uploadMax > 0 && $uploadMax < $maxSize) {
+                $maxSize = $uploadMax;
+            }
+        }
+
+        return $maxSize;
+    }
+
+    private function parseSize($size)
+    {
+        $unit = preg_replace('/[^bkmgtpezy]/i', '', $size); // Remove the non-unit characters from the size.
+        $size = preg_replace('/[^0-9\.]/', '', $size); // Remove the non-numeric characters from the size.
+        if ($unit) {
+            // Find the position of the unit in the ordered string which is the power of magnitude to multiply a kilobyte by.
+            return round($size * pow(1024, stripos('bkmgtpezy', $unit[0])));
+        }
+        else {
+            return round($size);
+        }
+    }
 }
