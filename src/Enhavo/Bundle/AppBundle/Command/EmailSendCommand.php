ba1efb0bced771bd637fa6f82d9b0b2708069320
FabianLiebl <fabian.liebl@xq-web.de>
2023-02-03 09:10:42 +0100

    [AppBundle] Added command to send email (#1732)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | no
    | New feature?  | yes
    | Doc PR?       | no
    | Backport      | 0.11
    | License       | MIT
    
    Added simple command "enhavo:email:send" to debug email dispatch
    
    (cherry picked from commit fd2b8f617d059c940df8d9d0238d12f6723883f8)

---
+++ src/Enhavo/Bundle/AppBundle/Command/EmailSendCommand.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Enhavo\Bundle\AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class EmailSendCommand extends Command
+{
+    public function __construct(
+        private MailerInterface $mailer,
+        private array $mailConfig
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure() {
+        $this
+            ->setName('enhavo:email:send')
+            ->setDescription('Debug command for testing email dispatch')
+            ->addArgument('from', InputArgument::OPTIONAL, 'From')
+            ->addArgument('to', InputArgument::OPTIONAL, 'To')
+            ->addArgument('subject', InputArgument::OPTIONAL, 'Subject')
+            ->addArgument('body', InputArgument::OPTIONAL, 'Body')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+
+        $from = $input->getArgument('from');
+        $to = $input->getArgument('to');
+        $subject = $input->getArgument('subject');
+        $body = $input->getArgument('body');
+
+        if (!$from) {
+            $from = $helper->ask($input, $output, new Question('From [' . $this->mailConfig['from'] . ']: ', $this->mailConfig['from']));
+        }
+        if (!$to) {
+            $to = $helper->ask($input, $output, new Question('To [' . $this->mailConfig['to'] . ']: ', $this->mailConfig['to']));
+        }
+        if (!$subject) {
+            $subject = $helper->ask($input, $output, new Question('Subject [Testmail]: ', 'Testmail'));
+        }
+        if (!$body) {
+            $body = $helper->ask($input, $output, new Question('Body [Testmail]: ', 'Testmail'));
+        }
+
+        $email = new Email();
+        $email->from($from);
+        $email->to($to);
+        $email->subject($subject);
+        $email->html($body);
+
+        $this->mailer->send($email);
+
+        return 0;
+    }
+}
