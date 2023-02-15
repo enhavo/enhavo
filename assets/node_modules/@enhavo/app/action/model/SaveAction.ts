5de2f4ad0c70dc2ffaf67d6d56a1338b30922276
Requiem4aPr0 <philipp.sester@xq-web.de>
2023-02-15 17:28:32 +0100

    remove loading screen after shop transition action

--- assets/node_modules/@enhavo/app/action/model/SaveAction.ts
+++ assets/node_modules/@enhavo/app/action/model/SaveAction.ts
@@ -3,6 +3,7 @@ import AbstractAction from "@enhavo/app/action/model/AbstractAction";
 import LoadingEvent from "@enhavo/app/view-stack/event/LoadingEvent";
 import View from "@enhavo/app/view/View";
 import EventDispatcher from "@enhavo/app/view-stack/EventDispatcher";
+import * as URI from "urijs";
 
 export default class SaveAction extends AbstractAction
 {
@@ -25,9 +26,11 @@ export default class SaveAction extends AbstractAction
         let $form = $('form');
 
         if (this.url) {
-            $form.attr('action', this.url);
+            let uri = new URI(this.url);
+            uri = uri.addQuery('view_id', this.view.getId());
+            $form.attr('action', uri);
         }
-
+        
         $form.submit();
     }
 }
