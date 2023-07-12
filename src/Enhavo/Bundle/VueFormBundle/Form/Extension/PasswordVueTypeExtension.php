4363069bb3882aec855c9bbaeeb2db30aba1da05
Gerhard Seidel <gseidel.message@googlemail.com>
2023-07-12 10:25:54 +0200

    fix: vue form password type (#1809)
    | Q             | A
    | ------------- | ---
    | Bug fix?      | yes
    | New feature?  | no
    | Doc PR?       | yes
    | License       | MIT
    
    * Fix vue password type

--- src/Enhavo/Bundle/VueFormBundle/Form/Extension/PasswordVueTypeExtension.php
+++ src/Enhavo/Bundle/VueFormBundle/Form/Extension/PasswordVueTypeExtension.php
@@ -3,11 +3,18 @@
 namespace Enhavo\Bundle\VueFormBundle\Form\Extension;
 
 use Enhavo\Bundle\VueFormBundle\Form\AbstractVueTypeExtension;
+use Enhavo\Bundle\VueFormBundle\Form\VueData;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PasswordVueTypeExtension extends AbstractVueTypeExtension
 {
+    public function buildVueData(FormView $view, VueData $data, array $options)
+    {
+        $data['type'] = 'password';
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
