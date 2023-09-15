7eccdeb796fc4838e0bbc3d6e5f7890ecc6c35ad
Gerhard Seidel <gseidel.message@googlemail.com>
2023-09-15 10:02:49 +0200

    feat: add endpoint extension type (#1839)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | yes
    | BC-Break?    | no
    | License      | MIT
    
    Allow type extensions for endpoints

---
+++ src/App/Endpoint/Extension/HelloEndpointExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Endpoint\Extension;
+
+use App\Endpoint\HelloEndpoint;
+use Enhavo\Bundle\ApiBundle\Endpoint\AbstractEndpointTypeExtension;
+use Enhavo\Bundle\ApiBundle\Endpoint\Context;
+use Enhavo\Bundle\ApiBundle\Endpoint\Data;
+use Symfony\Component\HttpFoundation\Request;
+
+class HelloEndpointExtension extends AbstractEndpointTypeExtension
+{
+    public function handleRequest($options, Request $request, Data $data, Context $context)
+    {
+        $data->set('extension', 'Here is a extension');
+    }
+
+    public static function getExtendedTypes(): array
+    {
+        return [HelloEndpoint::class];
+    }
+}
