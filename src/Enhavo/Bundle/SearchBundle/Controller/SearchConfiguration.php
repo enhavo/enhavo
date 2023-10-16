cad6b0467e9f3c96bb902bad6a56b31ceab89d1d
Gerhard Seidel <gseidel.message@googlemail.com>
2023-10-16 16:05:49 +0200

    feat: add static search filter config (#1858)
    | Q            | A
    |--------------| ---
    | Bug fix?     | yes
    | New feature? | yes
    | BC-Break?    | no
    | Backport     | 0.11 0.12 0.13
    | License      | MIT
    
    * Allow to define static filter in search route
    * Add a static public filter to fix showing unpublished content
    
    # Conflicts:
    #	src/Enhavo/Bundle/SearchBundle/Controller/SearchController.php
    #	src/Enhavo/Bundle/SearchBundle/Resources/config/services/services.yaml

--- src/Enhavo/Bundle/SearchBundle/Controller/SearchConfiguration.php
+++ src/Enhavo/Bundle/SearchBundle/Controller/SearchConfiguration.php
@@ -4,41 +4,37 @@ namespace Enhavo\Bundle\SearchBundle\Controller;
 
 class SearchConfiguration
 {
-    /** @var ?string */
-    private $template;
+    private ?string $template;
+    private ?int $maxPerPage = 10;
+    private array $filters = [];
 
-    /** @var ?int */
-    private $maxPerPage = 10;
-
-    /**
-     * @return string|null
-     */
     public function getTemplate(): ?string
     {
         return $this->template;
     }
 
-    /**
-     * @param string|null $template
-     */
     public function setTemplate(?string $template): void
     {
         $this->template = $template;
     }
 
-    /**
-     * @return int|null
-     */
     public function getMaxPerPage(): ?int
     {
         return $this->maxPerPage;
     }
 
-    /**
-     * @param int|null $maxPerPage
-     */
     public function setMaxPerPage(?int $maxPerPage): void
     {
         $this->maxPerPage = $maxPerPage;
+    }
+
+    public function getFilters(): array
+    {
+        return $this->filters;
+    }
+
+    public function setFilters(array $filters): void
+    {
+        $this->filters = $filters;
     }
 }
