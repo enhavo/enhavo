295d9cd98f9c3749a3e7824921f03572b372c53f
Gerhard Seidel <gseidel.message@googlemail.com>
2023-10-23 14:51:22 +0200

    feat: expose routes for endpoints

--- src/Enhavo/Bundle/AppBundle/Endpoint/Type/AreaEndpointType.php
+++ src/Enhavo/Bundle/AppBundle/Endpoint/Type/AreaEndpointType.php
@@ -2,7 +2,11 @@
 
 namespace Enhavo\Bundle\AppBundle\Endpoint\Type;
 
+use Enhavo\Bundle\ApiBundle\Data\Data;
 use Enhavo\Bundle\ApiBundle\Endpoint\AbstractEndpointType;
+use Enhavo\Bundle\ApiBundle\Endpoint\Context;
+use Enhavo\Bundle\AppBundle\Routing\RouteCollectorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Twig\Environment;
 
@@ -10,7 +14,14 @@ class AreaEndpointType extends AbstractEndpointType
 {
     public function __construct(
         private Environment $twig,
+        private RouteCollectorInterface $routeCollector,
     ) {}
+
+    public function handleRequest($options, Request $request, Data $data, Context $context)
+    {
+        $routesCollection = $this->routeCollector->getRouteCollection($options['area']);
+        $data->set('routes', $this->normalize($routesCollection, null, ['groups' => 'endpoint']));
+    }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
---
+++ src/Enhavo/Bundle/AppBundle/Routing/RouteCollectorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Enhavo\Bundle\AppBundle\Routing;
+
+use Symfony\Component\Routing\RouteCollection;
+
+interface RouteCollectorInterface
+{
+    public function getRouteCollection(null|string|array $groups): RouteCollection;
+}
