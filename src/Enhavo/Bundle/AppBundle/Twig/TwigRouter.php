ba7c80ed6fc85fa16e67e8b64f8c4d54fdc21bdd
Gerhard Seidel <gseidel.message@googlemail.com>
2023-10-30 12:58:12 +0100

    fix: init twig router

--- src/Enhavo/Bundle/AppBundle/Twig/TwigRouter.php
+++ src/Enhavo/Bundle/AppBundle/Twig/TwigRouter.php
@@ -10,24 +10,41 @@ use Symfony\Component\Routing\RouteCollection;
 
 class TwigRouter
 {
-    private UrlGenerator $generator;
-    private RouteCollection $routes;
+    private ?UrlGenerator $generator = null;
+    private ?RouteCollection $routes = null;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(
+        private readonly RequestStack $requestStack
+    )
     {
+    }
+
+    private function init(): void
+    {
+        if ($this->generator !== null) {
+            return;
+        }
+
         $context = new RequestContext();
-        $context->fromRequest($requestStack->getCurrentRequest());
+
+        $request = $this->requestStack->getCurrentRequest();
+        if ($request) {
+            $context->fromRequest($request);
+        }
+
         $this->routes = new RouteCollection();
         $this->generator = new UrlGenerator($this->routes, $context);
     }
 
     public function generate(string $name, array $parameters = [], int $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH): ?string
     {
+        $this->init();
         return $this->generator->generate($name, $parameters, $referenceType);
     }
 
-    public function addRouteCollection(RouteCollection $routeCollection)
+    public function addRouteCollection(RouteCollection $routeCollection): void
     {
+        $this->init();
         $this->routes->addCollection($routeCollection);
     }
 }
