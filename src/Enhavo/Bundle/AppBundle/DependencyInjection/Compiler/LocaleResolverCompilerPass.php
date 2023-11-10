19ddeffbe9561f93ce8cdf91a5e2450c14936660
Gerhard Seidel <gseidel.message@googlemail.com>
2023-11-10 11:47:47 +0100

    fix: setting locale

--- src/Enhavo/Bundle/AppBundle/DependencyInjection/Compiler/LocaleResolverCompilerPass.php
+++ src/Enhavo/Bundle/AppBundle/DependencyInjection/Compiler/LocaleResolverCompilerPass.php
@@ -3,6 +3,7 @@
 namespace Enhavo\Bundle\AppBundle\DependencyInjection\Compiler;
 
 use Enhavo\Bundle\AppBundle\EventListener\LocaleSubscriber;
+use Enhavo\Bundle\AppBundle\Locale\LocaleResolverInterface;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -12,7 +13,7 @@ class LocaleResolverCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $resolver = $container->getParameter('enhavo_app.locale_resolver');
-        $container->getDefinition(LocaleSubscriber::class)->replaceArgument(0, $container->getDefinition($resolver));
         $container->setAlias('enhavo_app.locale_resolver', new Alias($resolver, true));
+        $container->setAlias(LocaleResolverInterface::class, new Alias($resolver, true));
     }
 }
