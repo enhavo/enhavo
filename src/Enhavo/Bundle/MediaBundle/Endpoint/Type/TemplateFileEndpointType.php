879ed589f3d4d9ca9c084e981e13883c53cdac9a
Gerhard Seidel <gseidel.message@googlemail.com>
2023-11-13 21:19:36 +0100

    fix: move template file endpoint to media bundle

--- src/Enhavo/Bundle/MediaBundle/Endpoint/Type/TemplateFileEndpointType.php
+++ src/Enhavo/Bundle/MediaBundle/Endpoint/Type/TemplateFileEndpointType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Enhavo\Bundle\AppBundle\Endpoint\Type;
+namespace Enhavo\Bundle\MediaBundle\Endpoint\Type;
 
 use Enhavo\Bundle\ApiBundle\Data\Data;
 use Enhavo\Bundle\ApiBundle\Endpoint\AbstractEndpointType;
