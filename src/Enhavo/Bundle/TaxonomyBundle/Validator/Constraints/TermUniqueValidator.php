b4cd60e1874cd9eb051a50f0986769d74651c131
FabianLiebl <fabian.liebl@xq-web.de>
2023-11-24 11:38:20 +0100

    [TaxonomyBundle] Validator for unique term names (#1894)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | yes
    | BC-Break?    | no
    | License      | MIT
    
    Validator for unique term names

---
+++ src/Enhavo/Bundle/TaxonomyBundle/Validator/Constraints/TermUniqueValidator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Enhavo\Bundle\TaxonomyBundle\Validator\Constraints;
+
+use Enhavo\Bundle\TaxonomyBundle\Model\TermInterface;
+use Enhavo\Bundle\TaxonomyBundle\Repository\TermRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class TermUniqueValidator extends ConstraintValidator
+{
+    public function __construct(
+        private TermRepository $termRepository,
+    ) {}
+
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$constraint instanceof TermUnique) {
+            throw new UnexpectedTypeException($constraint, TermUnique::class);
+        }
+        if (!($value instanceof TermInterface)) {
+            return;
+        }
+        if (!$value->getName()) {
+            return;
+        }
+
+        $similarTerms = $this->termRepository->findBy([
+            'name' => $value->getName(),
+            'taxonomy' => $value->getTaxonomy()
+        ]);
+        $isDuplicate = false;
+        foreach ($similarTerms as $similarTerm) {
+            if ($similarTerm->getId() !== $value->getId()) {
+                $isDuplicate = true;
+                break;
+            }
+        }
+
+        if ($isDuplicate) {
+            $this->context
+                ->buildViolation($constraint->message)
+                ->setTranslationDomain($constraint->translationDomain)
+                ->atPath('name')
+                ->addViolation();
+        }
+    }
+}
