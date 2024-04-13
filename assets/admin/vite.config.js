9909628873424081464c18598ff4b0445fdbc9d3
Gerhard Seidel <gseidel.message@googlemail.com>
2024-04-13 11:58:27 +0800

    fix: import vite js and css

--- assets/admin/vite.config.js
+++ assets/admin/vite.config.js
@@ -5,10 +5,11 @@ import path from 'node:path'
 import 'dotenv/config'
 import containerDIPlugin from '@enhavo/dependency-injection/rollup-plugin-container-di'
 import {fantasticon} from "@enhavo/app/vite/fantasticon-plugin/plugin.js";
+import {watchNodeModules} from "@enhavo/app/vite/watch-node-modules-plugin.js";
 
 export default defineConfig({
     optimizeDeps: {
-        include: ['axios', 'uuid/v4', 'vuedraggable', 'jquery'],
+        include: ['axios', 'uuid/v4', 'vuedraggable', 'jquery', 'async', 'pako', 'lodash', 'ansi-to-html', 'jexl'],
     },
     plugins: [
         vue(),
@@ -26,6 +27,32 @@ export default defineConfig({
             prefix: 'icon',
             tag: '*'
         }),
+        watchNodeModules([
+            '@enhavo/app',
+            '@enhavo/api',
+            '@enhavo/app',
+            '@enhavo/article',
+            '@enhavo/block',
+            '@enhavo/calendar',
+            '@enhavo/contact',
+            '@enhavo/content',
+            '@enhavo/core',
+            '@enhavo/dashboard',
+            '@enhavo/dependency-injection',
+            '@enhavo/form',
+            '@enhavo/media',
+            '@enhavo/media-library',
+            '@enhavo/multi-tenancy',
+            '@enhavo/newsletter',
+            '@enhavo/payment',
+            '@enhavo/search',
+            '@enhavo/shop',
+            '@enhavo/slider',
+            '@enhavo/theme',
+            '@enhavo/translation',
+            '@enhavo/user',
+            '@enhavo/vue-form',
+        ]),
     ],
 
     // config
@@ -50,7 +77,9 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            vue: 'vue/dist/vue.esm-bundler.js'
+            vue: 'vue/dist/vue.esm-bundler.js',
+            '$':  'jQuery',
         }
     }
+
 })
