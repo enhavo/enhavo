d98200c68c55a7aff7f0e19374a45b853ae7eeac
Gerhard Seidel <gseidel.message@googlemail.com>
2024-04-23 17:42:37 +0800

    fix: tinymce in wysiwyg form

--- assets/node_modules/@enhavo/form/form/model/WysiwygForm.ts
+++ assets/node_modules/@enhavo/form/form/model/WysiwygForm.ts
@@ -1,15 +1,24 @@
 import {Form} from "@enhavo/vue-form/model/Form";
-import tinymce from 'tinymce/tinymce';
-import 'tinymce/icons/default';
-import 'tinymce/themes/silver';
-import 'tinymce/skins/ui/oxide/skin.css';
-import 'tinymce/plugins/advlist';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/emoticons';
-import 'tinymce/plugins/emoticons/js/emojis';
-import 'tinymce/plugins/link';
-import 'tinymce/plugins/lists';
-import 'tinymce/plugins/table';
+import tinymce from "tinymce";
+
+import "tinymce/icons/default/icons.min.js";
+
+/* Required TinyMCE components */
+import "tinymce/themes/silver/theme.min.js";
+import "tinymce/models/dom/model.min.js";
+
+/* Import a skin (can be a custom skin instead of the default) */
+import "tinymce/skins/ui/oxide/skin.js";
+
+// /* Import plugins */
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/code";
+import "tinymce/plugins/emoticons";
+import "tinymce/plugins/emoticons/js/emojis";
+import "tinymce/plugins/link";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/table";
+
 import {MoveEvent} from "@enhavo/form/form/event/MoveEvent";
 
 export class WysiwygForm extends Form
