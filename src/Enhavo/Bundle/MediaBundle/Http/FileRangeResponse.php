ca05ad3d67a82ea111ab083e745095ab34341452
Benjamin Lutze <bl@weareindeed.com>
2024-05-15 16:54:07 +0200

    fix wrongly configured header

--- src/Enhavo/Bundle/MediaBundle/Http/FileRangeResponse.php
+++ src/Enhavo/Bundle/MediaBundle/Http/FileRangeResponse.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @author blutze-media
+ * @since 2024-05-15
+ */
 
 namespace Enhavo\Bundle\MediaBundle\Http;
 
@@ -8,23 +12,27 @@ use Symfony\Component\HttpFoundation\Response;
 class FileRangeResponse extends Response
 {
 
-    public function __construct(FileInterface $file, int $length, int $start, int $end)
+    public function __construct(FileInterface $file, int $start, int $end)
     {
         parent::__construct();
         $this->setStatusCode(Response::HTTP_PARTIAL_CONTENT);
-        $this->setFile($file, $length, $start, $end);
+        $this->setFile($file, $start, $end);
     }
 
-    public function setFile(FileInterface $file, int $length, int $start, int $end): void
+    public function setFile(FileInterface $file, int $start, int $end): void
     {
-        $handle = fopen($file->getContent()->getFilePath(), 'rb');
+        $path = $file->getContent()->getFilePath();
+        $size = filesize($path);
+        $length = $end - $start + 1;
+
+        $handle = fopen($path, 'rb');
         // Seek to the starting position and read the specified length
         fseek($handle, $start);
         $content = fread($handle, $length);
         fclose($handle);
 
         $this->setContent($content);
-        $this->headers->set('Content-Range', sprintf('bytes %d-%d/%d', $start, $end, $length));
+        $this->headers->set('Content-Range', sprintf('bytes %d-%d/%d', $start, $end, $size));
         $this->headers->set('Accept-Ranges', '0-' . $length);
         $this->headers->set('Content-Length', $length);
         $this->headers->set('Content-Type', $file->getMimeType());
