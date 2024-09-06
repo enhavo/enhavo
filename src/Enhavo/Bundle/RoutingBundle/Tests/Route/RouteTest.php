22cfdd211231969aab477f42f2f6803b6ee48950
Benjamin Lutze <bl@weareindeed.com>
2024-09-06 08:10:09 +0200

    cleanup

--- src/Enhavo/Bundle/RoutingBundle/Tests/Route/RouteTest.php
+++ src/Enhavo/Bundle/RoutingBundle/Tests/Route/RouteTest.php
@@ -3,27 +3,17 @@
 namespace Route;
 
 use Enhavo\Bundle\RoutingBundle\Entity\Route;
-use Enhavo\Bundle\RoutingBundle\Tests\Mock\RouteContentMock;
 use PHPUnit\Framework\TestCase;
 
 class RouteTest extends TestCase
 {
-    private function createResource()
-    {
-        $resource = new RouteContentMock();
-        $resource->setRoute(new Route());
-        $resource->setTitle('this is a title');
-        $resource->setSubTitle('My subtitle');
-        return $resource;
-    }
-
     public function testResetPath()
     {
-        $resource = $this->createResource();
-        $resource->getRoute()->setPath('/exists');
-        $this->assertEquals('/exists', $resource->getRoute()->getStaticPrefix());
-        $resource->getRoute()->setPath(null);
-        $this->assertEquals('', $resource->getRoute()->getStaticPrefix());
+        $route = new Route();
+        $route->setPath('/exists');
+        $this->assertEquals('/exists', $route->getStaticPrefix());
+        $route->setPath(null);
+        $this->assertEquals('', $route->getStaticPrefix());
     }
 
 }
