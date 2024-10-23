f81367de8a7b92ea34cb43680088a7c87b86b20e
Benjamin Lutze <bl@weareindeed.com>
2024-10-23 09:03:27 +0200

    [RoutingBundle] change order of transliteration and urlization to make it even work wit arabic

--- src/Enhavo/Bundle/RoutingBundle/Slugifier/Slugifier.php
+++ src/Enhavo/Bundle/RoutingBundle/Slugifier/Slugifier.php
@@ -13,8 +13,8 @@ class Slugifier implements SlugifierInterface
     public static function slugify($content, $separator = '-')
     {
         $urlizer = new Urlizer();
-        $content = $urlizer->urlize($content, $separator);
         $content  = $urlizer->transliterate($content, $separator);
+        $content = $urlizer->urlize($content, $separator);
         return $content;
     }
 }
