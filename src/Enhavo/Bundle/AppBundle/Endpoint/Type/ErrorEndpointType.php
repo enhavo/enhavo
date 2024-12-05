ffc98de7337e811406615bdca40d30e0c17470b8
Gerhard Seidel <gseidel.message@googlemail.com>
2024-12-05 10:46:10 +0100

    feat: error endpoint type

---
+++ src/Enhavo/Bundle/AppBundle/Endpoint/Type/ErrorEndpointType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Enhavo\Bundle\AppBundle\Endpoint\Type;
+
+use Enhavo\Bundle\ApiBundle\Data\Data;
+use Enhavo\Bundle\ApiBundle\Endpoint\AbstractEndpointType;
+use Enhavo\Bundle\ApiBundle\Endpoint\Context;
+use Enhavo\Bundle\AppBundle\Template\TemplateResolver;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Twig\Environment;
+
+class ErrorEndpointType extends AbstractEndpointType
+{
+    public function __construct(
+        private readonly TemplateResolver $templateResolver,
+        private readonly Environment $twigEnvironment,
+    )
+    {
+    }
+
+    public function handleRequest($options, Request $request, Data $data, Context $context): void
+    {
+        $data['status_code'] = $options['status_code'];
+        $data['status_text'] = $options['status_text'];
+
+        $template = $this->templateResolver->resolve(sprintf('theme/error/%s.html.twig', $options['status_code']));
+        if ($this->twigEnvironment->getLoader()->exists($template)) {
+            $context->set('template', $template);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'template' => 'theme/error/default.html.twig',
+            'exception' => null,
+            'status_code' => null,
+            'status_text' => null,
+        ]);
+    }
+
+    public static function getParentType(): ?string
+    {
+        return AreaEndpointType::class;
+    }
+}
