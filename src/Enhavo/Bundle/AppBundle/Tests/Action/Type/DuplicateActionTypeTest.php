15d18bcf44b655200a5e1029f7ef3ce0951d39b5
Gerhard Seidel <gseidel.message@googlemail.com>
2024-12-10 09:22:48 +0100

    fix: tests

--- src/Enhavo/Bundle/AppBundle/Tests/Action/Type/DuplicateActionTypeTest.php
+++ src/Enhavo/Bundle/AppBundle/Tests/Action/Type/DuplicateActionTypeTest.php
@@ -9,11 +9,15 @@
 namespace Enhavo\Bundle\AppBundle\Tests\Action\Type;
 
 use Enhavo\Bundle\AppBundle\Action\Type\DuplicateActionType;
+use Enhavo\Bundle\ResourceBundle\ExpressionLanguage\ResourceExpressionLanguage;
+use Enhavo\Bundle\ResourceBundle\RouteResolver\RouteResolverInterface;
 use Enhavo\Bundle\ResourceBundle\Tests\Mock\ResourceMock;
 use Enhavo\Bundle\ResourceBundle\Action\Action;
 use Enhavo\Bundle\ResourceBundle\Tests\Action\Type\BaseActionTypeFactoryTrait;
+use Enhavo\Bundle\ResourceBundle\Tests\Mock\RouterMock;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
@@ -26,17 +30,27 @@ class DuplicateActionTypeTest extends TestCase
     {
         $dependencies = new DuplicateActionTypeDependencies();
         $dependencies->tokenManager = $this->getMockBuilder(CsrfTokenManagerInterface::class)->getMock();
+        $dependencies->router = new RouterMock();
+        $dependencies->routeResolver = $this->getMockBuilder(RouteResolverInterface::class)->getMock();
+        $dependencies->expressionLanguage = new ResourceExpressionLanguage();
         return $dependencies;
     }
 
     private function createInstance(DuplicateActionTypeDependencies $dependencies)
     {
-        return new DuplicateActionType($dependencies->tokenManager);
+        return new DuplicateActionType(
+            $dependencies->router,
+            $dependencies->routeResolver,
+            $dependencies->expressionLanguage,
+            $dependencies->tokenManager,
+        );
     }
 
     public function testCreateViewData()
     {
         $dependencies = $this->createDependencies();
+        $dependencies->routeResolver->method('getRoute')->willReturn('duplicate_route');
+
         $dependencies->tokenManager->method('getToken')->willReturn(new CsrfToken('id123', 'value123'));
         $type = $this->createInstance($dependencies);
 
@@ -64,4 +78,7 @@ class DuplicateActionTypeTest extends TestCase
 class DuplicateActionTypeDependencies
 {
     public CsrfTokenManagerInterface|MockObject $tokenManager;
+    public RouterInterface|MockObject $router;
+    public RouteResolverInterface|MockObject $routeResolver;
+    public ResourceExpressionLanguage|MockObject $expressionLanguage;
 }
