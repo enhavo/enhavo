086c4977b614261b7e7f6b8def16018ffeaf5311
Fabian Liebl <fabian.liebl@xq-web.de>
2025-01-28 14:32:27 +0100

    [FormBundle] Form type PolyCollection: AddButton at more position, text filter for add button items

--- assets/node_modules/@enhavo/form/components/FormPolyCollection.vue
+++ assets/node_modules/@enhavo/form/components/FormPolyCollection.vue
@@ -1,5 +1,9 @@
 <template>
     <div class="form-poly-collection" :ref="(el) => form.setElement(el as HTMLElement)" v-show="form.isVisible()">
+        <slot name="button-row">
+            <form-poly-collection-add-button :form="form" :add-after="null"></form-poly-collection-add-button>
+        </slot>
+
         <slot name="list">
             <draggable
                 v-model="form.children"
@@ -13,48 +17,37 @@
                 class="form-poly-collection-items"
             >
                 <template #item="{ element }">
-                    <component
-                        :is="form.itemComponent"
-                        :form="element"
-                        :deletable="form.allowDelete"
-                        :sortable="form.sortable"
-                        :collapsable="form.collapsable"
-                        :collapsed="form.isCollapsed(element)"
-                        :block-name="form.blockName"
-                        @delete="event => { form.deleteItem(event)} "
-                        @up="event => { form.moveItemUp(event) }"
-                        @down="event => { form.moveItemDown(event) }"
-                        @uncollapse="event => { form.uncollapse(element) }"
-                        @collapse="event => { form.collapse(element) }"
-                    >
-                        <template v-slot>
-                            <slot name="item"></slot>
-                        </template>
-                        <template v-slot:buttons><slot name="item-buttons"></slot></template>
-                        <template v-slot:down-button><slot name="item-down-button"></slot></template>
-                        <template v-slot:up-button><slot name="item-up-button"></slot></template>
-                        <template v-slot:delete-button><slot name="item-delete-button"></slot></template>
-                        <template v-slot:drag-button><slot name="item-drag-button"></slot></template>
-                    </component>
-                </template>
-            </draggable>
-        </slot>
+                    <div>
+                        <component
+                            :is="form.itemComponent"
+                            :form="element"
+                            :deletable="form.allowDelete"
+                            :sortable="form.sortable"
+                            :collapsable="form.collapsable"
+                            :collapsed="form.isCollapsed(element)"
+                            :block-name="form.blockName"
+                            @delete="event => { form.deleteItem(event)} "
+                            @up="event => { form.moveItemUp(event) }"
+                            @down="event => { form.moveItemDown(event) }"
+                            @uncollapse="event => { form.uncollapse(element) }"
+                            @collapse="event => { form.collapse(element) }"
+                        >
+                            <template v-slot>
+                                <slot name="item"></slot>
+                            </template>
+                            <template v-slot:buttons><slot name="item-buttons"></slot></template>
+                            <template v-slot:down-button><slot name="item-down-button"></slot></template>
+                            <template v-slot:up-button><slot name="item-up-button"></slot></template>
+                            <template v-slot:delete-button><slot name="item-delete-button"></slot></template>
+                            <template v-slot:drag-button><slot name="item-drag-button"></slot></template>
+                        </component>
 
-        <slot name="button-row">
-            <div class="form-poly-collection-add-button-row" v-if="form.allowAdd">
-                <slot name="buttons">
-                    <div class="form-poly-collection-add-button" @click.prevent="form.toggleMenu()">
-                        <slot name="add-button">
-                            <i class="icon icon-add"></i>
+                        <slot name="button-row">
+                            <form-poly-collection-add-button :form="form" :add-after="element"></form-poly-collection-add-button>
                         </slot>
                     </div>
-                </slot>
-                <div v-if="form.isOpen" class="form-poly-collection-add-menu">
-                    <template v-for="entryLabel of form.entryLabels">
-                        <div v-if="isAllowed(entryLabel.key)" @click.prevent="form.addItem(entryLabel.key); form.toggleMenu()" class="add-menu-item">{{ entryLabel.label }}</div>
-                    </template>
-                </div>
-            </div>
+                </template>
+            </draggable>
         </slot>
     </div>
 </template>
@@ -62,15 +55,11 @@
 <script setup lang="ts">
 import {PolyCollectionForm} from "@enhavo/form/form/model/PolyCollectionForm";
 import * as draggableComponent from 'vuedraggable'
+import FormPolyCollectionAddButton from "@enhavo/form/components/FormPolyCollectionAddButton.vue";
 
 const draggable = draggableComponent;
 const props = defineProps<{
     form: PolyCollectionForm
 }>()
-
-function isAllowed(key: string): boolean
-{
-    return props.form.entryKeys.indexOf(key) >= 0;
-}
 
 </script>
