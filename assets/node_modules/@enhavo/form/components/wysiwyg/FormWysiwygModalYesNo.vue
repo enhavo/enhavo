84f4caa197c0aa9395840782820088ba00805351
FabianLiebl <fabian.liebl@xq-web.de>
2025-02-17 13:53:04 +0100

    [FormBundle] Add more table icons; add prompt before deleting table (#2195)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | yes
    | BC-Break?    | no
    | License      | MIT
    
    [FormBundle] Add more table icons; add prompt before deleting table

---
+++ assets/node_modules/@enhavo/form/components/wysiwyg/FormWysiwygModalYesNo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="wysiwyg-modal">
+        <div class="wysiwyg-modal-header">
+            {{ form.modal.options.hasOwnProperty('headline') ? form.modal.options['headline'] : '' }}
+        </div>
+        <div class="wysiwyg-modal-content"
+             v-html="form.modal.options.hasOwnProperty('prompt') ? form.modal.options['prompt'] : translator.trans('enhavo_form.wysiwyg_form.command.modal_yes_no.default_prompt', [], 'javascript')"
+        >
+        </div>
+        <div class="wysiwyg-modal-actions">
+            <a class="btn wysiwyg-modal-action" @click.prevent="submit">
+                {{ form.modal.options.hasOwnProperty('label_yes') ? form.modal.options['label_yes'] : translator.trans('enhavo_form.wysiwyg_form.command.modal_yes_no.default_yes', [], 'javascript') }}
+            </a>
+            <a class="btn-secondary wysiwyg-modal-action" @click.prevent="cancel">
+                {{ form.modal.options.hasOwnProperty('label_no') ? form.modal.options['label_no'] : translator.trans('enhavo_form.wysiwyg_form.command.modal_yes_no.default_no', [], 'javascript') }}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {WysiwygForm} from "@enhavo/form/form/model/WysiwygForm";
+import {inject} from "vue";
+import {Translator} from "@enhavo/app/translation/Translator";
+
+const translator = inject<Translator>('translator');
+
+const props = defineProps<{
+    form: WysiwygForm
+}>()
+
+function submit()
+{
+    props.form.modal.submit({});
+}
+
+function cancel()
+{
+    props.form.modal.cancel();
+}
+
+</script>
