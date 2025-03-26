720a3d7cb153ce1e151ebd0a4b1055f14d31d99c
FabianLiebl <fabian.liebl@xq-web.de>
2025-03-26 14:52:04 +0100

    [AppBundle] Fix AppBundle ModalController using ->get() Controller method which no longer exists in this Version of Symfony (#2236)
    | Q            | A
    |--------------| ---
    | Bug fix?     | yes
    | New feature? | no
    | BC-Break?    | no
    | License      | MIT
    
    [AppBundle] Fix AppBundle ModalController using $this->get() Controller method which no longer exists in this Version of Symfony

--- src/Enhavo/Bundle/AppBundle/Controller/ModalController.php
+++ src/Enhavo/Bundle/AppBundle/Controller/ModalController.php
@@ -10,6 +10,7 @@ namespace Enhavo\Bundle\AppBundle\Controller;
 
 use Enhavo\Bundle\AppBundle\Template\TemplateResolverTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,10 +18,14 @@ class ModalController extends AbstractController
 {
     use TemplateResolverTrait;
 
+    public function __construct(
+        private FormFactoryInterface $formFactory
+    ) {}
+
     public function formAction(Request $request)
     {
         $configuration = $this->createConfiguration($request);
-        $form = $this->get('form.factory')->create($configuration->getForm(), null, $configuration->getOptions());
+        $form = $this->formFactory->create($configuration->getForm(), null, $configuration->getOptions());
         $template = $configuration->getTemplate() ? $configuration->getTemplate() : 'admin/view/modal-form.html.twig';
 
         $form->handleRequest($request);
