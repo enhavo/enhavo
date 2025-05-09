60d81cbe42cf1023f3c8e828d2597e10b5175e8e
Benjamin Lutze <bl@weareindeed.com>
2025-05-09 06:47:34 +0200

    [RevisionBundle] Add Revision Filter Hook for Commands

--- src/Enhavo/Bundle/RevisionBundle/EventListener/RevisionDoctrineFilterListener.php
+++ src/Enhavo/Bundle/RevisionBundle/EventListener/RevisionDoctrineFilterListener.php
@@ -16,9 +16,29 @@ class RevisionDoctrineFilterListener
 
     public function onKernelRequest(): void
     {
-        if ($this->enabled) {
-            $this->entityManager->getConfiguration()->addFilter('revision', RevisionFilter::class);
-            $this->entityManager->getFilters()->enable('revision');
+        $this->enableFilter();
+    }
+
+    public function onConsoleCommand(): void
+    {
+        $this->enableFilter();
+    }
+
+    private function enableFilter(): void
+    {
+        if (!$this->enabled) {
+            return;
+        }
+
+        $config = $this->entityManager->getConfiguration();
+
+        if (!$config->getFilterClassName('revision')) {
+            $config->addFilter('revision', RevisionFilter::class);
+        }
+
+        $filters = $this->entityManager->getFilters();
+        if (!$filters->isEnabled('revision')) {
+            $filters->enable('revision');
         }
     }
 }
