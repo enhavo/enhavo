cbd79a05eb55ab82436adcfdbfddf717c7e6704b
Gerhard Seidel <gs@weareindeed.com>
2026-01-09 11:07:45 +0800

    chore: add subtree split and npm release as dagger calls

--- .dagger/src/Basics.php
+++ .dagger/src/Basics.php
@@ -10,6 +10,7 @@ use Dagger\Attribute\DefaultPath;
 use Dagger\Attribute\Doc;
 use Dagger\Container;
 use Dagger\Directory;
+use Dagger\Secret;
 
 use function Dagger\dag;
 
@@ -44,10 +45,55 @@ class Basics
 
     #[DaggerFunction]
     #[Doc('Run phpunit')]
-    public function phpunit(): Container
+    public function phpunit(string $args = ''): Container
     {
         return $this->setupPhpEnv()
-            ->withExec(['bin/phpunit'])
+            ->withExec(['bin/phpunit', $args])
+        ;
+    }
+
+    #[DaggerFunction]
+    #[Doc('Push subtrees')]
+    public function pushSubtrees(string $branch): Container
+    {
+        return dag()
+            ->container()
+            ->from('php:8.3-cli-alpine')
+            ->withMountedDirectory('/app', $this->source)
+            ->withWorkdir('/app')
+            ->withExec(['curl', '-sLO', 'https://github.com/enhavo/enhavo-cli/releases/latest/download/enhavo.phar'])
+            ->withExec(['chmod', '+x', 'enhavo.phar'])
+            ->withExec(['apk', 'add', '--no-cache', 'git'])
+            ->withExec(['git', 'config', '--global', 'user.email', '"bot@enhavo.com"'])
+            ->withExec(['git', 'config', '--global', 'user.name', '"enhavo bot"'])
+            ->withExec(['enhavo.phar', 'push-subtree', '--yes', '-vvv', '--branch', escapeshellarg($branch)])
+            ->terminal()
+        ;
+    }
+
+    #[DaggerFunction]
+    #[Doc('npm release')]
+    public function npmRelease(string $version, Secret $token): Container
+    {
+        return dag()
+            ->container()
+            ->from('node:24-alpine')
+            ->withMountedDirectory('/app', $this->source)
+            ->withWorkdir('/app')
+            ->withSecretVariable('NPM_TOKEN', $token)
+            ->withExec(['apk', 'add', 'git', 'jq'])
+
+            ->withExec(['yarn', 'config', 'set', 'npmRegistryServer', 'https://registry.npmjs.org'])
+            ->withExec(['yarn', 'config', 'set', 'npmAlwaysAuth', 'true'])
+            ->withExec(['sh', '-c', 'yarn config set npmAuthToken $NPM_TOKEN'])
+
+            // Find all package.json files (excluding node_modules) and set .version everywhere
+            ->withExec([
+                'sh',
+                '-c',
+                'find /app/packages -name package.json -not -path \'*/node_modules/*\' -print0 | while IFS= read -r -d \'\' f; do jq --arg v '.escapeshellarg($version).' \'.version=$v\' "$f" > "$f.tmp" && mv "$f.tmp" "$f"; done'
+            ])
+            ->withExec(['yarn', 'workspaces', 'foreach', '-A', 'npm', 'publish', '--access', 'public'])
         ;
     }
 }
