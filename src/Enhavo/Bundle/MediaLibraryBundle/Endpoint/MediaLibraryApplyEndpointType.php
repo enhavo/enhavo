d071be638f002d8fd52409770c2b117884597724
visual.indi <7021685+indivisualvj@users.noreply.github.com>
2026-01-27 10:08:29 +0100

    [MediaLibraryBundle] feat: action to update basename, alt, title in usedFiles of item (#2366)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | yes
    | BC-Break?    | no
    | Backport     | 0.15
    | License      | MIT
    
    As meanwhile it is possible to replace the content of a file via MediaLibrary, what still was missing is the ability to also exchange filename, alt and title of all current files in use.
    
    (cherry picked from commit a09f11981c41fdad9b2b3566c4f3d20d55060aa9)

---
+++ src/Enhavo/Bundle/MediaLibraryBundle/Endpoint/MediaLibraryApplyEndpointType.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the enhavo package.
+ *
+ * (c) WE ARE INDEED GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Enhavo\Bundle\MediaLibraryBundle\Endpoint;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Enhavo\Bundle\ApiBundle\Data\Data;
+use Enhavo\Bundle\ApiBundle\Endpoint\AbstractEndpointType;
+use Enhavo\Bundle\ApiBundle\Endpoint\Context;
+use Enhavo\Bundle\MediaLibraryBundle\Model\ItemInterface;
+use Enhavo\Bundle\MediaLibraryBundle\Model\LibraryFileInterface;
+use Enhavo\Bundle\ResourceBundle\Authorization\Permission;
+use Enhavo\Bundle\ResourceBundle\Input\Input;
+use Enhavo\Bundle\ResourceBundle\Input\InputFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaLibraryApplyEndpointType extends AbstractEndpointType
+{
+    public function __construct(
+        private readonly InputFactory $inputFactory,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function handleRequest($options, Request $request, Data $data, Context $context): void
+    {
+        /** @var Input $input */
+        $input = $this->inputFactory->create($options['input']);
+
+        /** @var ItemInterface $resource */
+        $resource = $input->getResource();
+
+        if (null === $resource) {
+            throw $this->createNotFoundException();
+        }
+
+        $this->denyAccessUnlessGranted(new Permission($input->getResourceName(), $options['permission']), $resource);
+
+        /** @var LibraryFileInterface $file */
+        $file = $resource->getFile();
+        $used = $resource->getUsedFiles();
+        /** @var LibraryFileInterface $usedFile */
+        foreach ($used as $usedFile) {
+            $usedFile->setBasename($file->getBasename());
+            $usedFile->setParameters($file->getParameters());
+        }
+
+        $this->entityManager->flush();
+
+        $viewData = $input->getViewData($resource);
+        $data->add($viewData);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'permission' => Permission::UPDATE,
+        ]);
+
+        $resolver->setRequired('input');
+    }
+
+    public static function getName(): ?string
+    {
+        return 'media_library_apply';
+    }
+}
