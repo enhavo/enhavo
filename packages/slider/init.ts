1abd921d4adafd7e778516e484288c6b2b28354f
Gerhard Seidel <gs@weareindeed.com>
2026-01-27 19:59:49 +0800

    fix: bind jquery version to 3.7

--- packages/slider/init.ts
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-import '@enhavo/slider/assets/styles/block.scss';
-import SliderBlock from "@enhavo/slider/block/SliderBlock";
-
-$(() => {
-    (new SliderBlock).init(document.body);
-});
