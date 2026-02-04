f805e793e48bc60aecaf8debfe6570612d45c897
Gerhard Seidel <gs@weareindeed.com>
2026-02-04 14:41:54 +0800

    refactor: move app files to playground (#2377)
    | Q            | A
    |--------------| ---
    | Bug fix?     | no
    | New feature? | no
    | BC-Break?    | no
    | Backport     | no
    | License      | MIT
    
    Move all file needed for running the application into a playground subfolder. So it is possible to have different setups in this repository

---
+++ playground/app/src/Kernel.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the enhavo package.
+ *
+ * (c) WE ARE INDEED GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App;
+
+use Enhavo\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function getProjectDir(): string
+    {
+        return realpath(dirname(__DIR__));
+    }
+}
