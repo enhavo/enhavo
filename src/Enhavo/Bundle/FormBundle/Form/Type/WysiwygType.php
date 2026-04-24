41a170f609cf8b6cb4a3f6ea367fa7abdf918cb4
Gerhard Seidel <gs@weareindeed.com>
2026-04-24 13:37:52 +0800

    fix: api docs

--- src/Enhavo/Bundle/FormBundle/Form/Type/WysiwygType.php
+++ src/Enhavo/Bundle/FormBundle/Form/Type/WysiwygType.php
@@ -11,16 +11,19 @@
 
 namespace Enhavo\Bundle\FormBundle\Form\Type;
 
+use Enhavo\Bundle\ApiBundle\Documentation\Model\Schema;
+use Enhavo\Bundle\ResourceBundle\Form\FormTypeDescribeAwareInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * @author Gerhard Seidel <gseidel.message@googlemail.com>
  */
-class WysiwygType extends AbstractType
+class WysiwygType extends AbstractType implements FormTypeDescribeAwareInterface
 {
     public function buildView(FormView $view, FormInterface $form, array $options): void
     {
@@ -39,5 +42,11 @@ class WysiwygType extends AbstractType
     public function getParent()
     {
         return TextareaType::class;
+    }
+
+    public function describe($options, FormTypeInterface $form, Schema $schema)
+    {
+        $schema->string()
+            ->format('html');
     }
 }
