aef0553ea7f7bce83c26873f41ac92163af488eb
Gerhard Seidel <gs@weareindeed.com>
2026-04-27 19:32:54 +0800

    fix: js eval

--- packages/vue-form/model/Form.ts
+++ packages/vue-form/model/Form.ts
@@ -189,7 +189,7 @@ export class Form
                 .join(',');
             const argsDef = argsStr ? `let ${argsStr};` : '';
 
-            return eval(`${argsDef}${code}`);
+            return (0, eval)(`${argsDef}${code}`);
         }.call(args);
     }
 
