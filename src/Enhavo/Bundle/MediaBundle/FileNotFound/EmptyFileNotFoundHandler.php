5b03c43624fd436a08e8a7f7f76a30bccc8eb003
visual.indi <7021685+indivisualvj@users.noreply.github.com>
2026-05-12 14:52:53 +0200

    [MediaBundle] feat: Add empty file not found handler (#2416)
    | Q            | A
    |--------------| ---
    | Bug fix?     | yes
    | New feature? | yes
    | BC-Break?    | no
    | Backport     | 0.15
    | License      | MIT
    
    In order to let developers execute commands and test things where no files are involved, but an existing database is required, add the possibility to just return empty content if files are not found.
    
    (cherry picked from commit 07a3407236762b36ab4a4745dd3e08b7fefb5b9d)

---
+++ src/Enhavo/Bundle/MediaBundle/FileNotFound/EmptyFileNotFoundHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the enhavo package.
+ *
+ * (c) WE ARE INDEED GmbH
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Enhavo\Bundle\MediaBundle\FileNotFound;
+
+use Enhavo\Bundle\MediaBundle\Content\Content;
+use Enhavo\Bundle\MediaBundle\Exception\FileNotFoundException;
+use Enhavo\Bundle\MediaBundle\Model\FileInterface;
+use Enhavo\Bundle\MediaBundle\Model\FormatInterface;
+use Enhavo\Bundle\MediaBundle\Storage\StorageInterface;
+
+class EmptyFileNotFoundHandler implements FileNotFoundHandlerInterface
+{
+    public function handleSave(FormatInterface|FileInterface $file, StorageInterface $storage, FileNotFoundException $exception, array $parameters = []): void
+    {
+        // do nothing
+    }
+
+    public function handleLoad(FormatInterface|FileInterface $file, StorageInterface $storage, FileNotFoundException $exception, array $parameters = []): void
+    {
+        $file->setContent(new Content('', ''));
+    }
+
+    public function handleDelete(FormatInterface|FileInterface $file, StorageInterface $storage, FileNotFoundException $exception, array $parameters = []): void
+    {
+        // do nothing
+    }
+}
